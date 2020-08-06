c6b526d64f7a78e33e2a50d1da3608f78a0212ec
userdky <userdyk@gmail.com>
2020-08-06 19:11:50 +0900

    readme.md update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name                = 'ailever',
-      version             = '0.0.5',
+      version             = '0.0.6',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
