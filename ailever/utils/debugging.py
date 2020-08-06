cbece7585075ac148a7287bb8a7d7ed45b1a6736
userdky <userdyk@gmail.com>
2020-08-06 22:25:33 +0900

    debugging.py update

--- ailever/utils/debugging.py
+++ ailever/utils/debugging.py
@@ -1,4 +1,5 @@
 import sys, os
+import h5py
 from collections import OrderedDict
 import pprint
 import inspect
@@ -6,26 +7,30 @@ import re
 
 
 class Attribute:
-    def __init__(self, attrviewer=False, itercount=None, iterdepth=None):
+    def __init__(self, attrviewer=True, itercount=None, iterdepth=None):
         self.attrviewer = attrviewer
         self.itercount = itercount
         self.iterdepth = iterdepth
 
     def rc_get(self, obj, memory='', depth=0):
         if not self.iterdepth:
-            if hasattr(obj, '__dict__') and len(obj.__dict__) != 0:
+            if isinstance(obj, (h5py._hl.files.File, h5py._hl.group.Group, h5py._hl.dataset.Dataset)):
+                self._rc_get_hdf5(obj, memory, depth)
+            elif hasattr(obj, '__dict__') and len(obj.__dict__) != 0:
                 self._rc_get_obj(obj, memory, depth)
             elif isinstance(obj, list):
                 self._rc_get_list(obj, memory, depth)
-            elif isinstance(obj, dict):
+            elif isinstance(obj, (dict, OrderedDict)):
                 self._rc_get_dict(obj, memory, depth)
         else:
             if depth < self.iterdepth:
-                if hasattr(obj, '__dict__') and len(obj.__dict__) != 0:
+                if isinstance(obj, (h5py._hl.files.File, h5py._hl.group.Group, h5py._hl.dataset.Dataset)):
+                    self._rc_get_hdf5(obj, memory, depth)
+                elif hasattr(obj, '__dict__') and len(obj.__dict__) != 0:
                     self._rc_get_obj(obj, memory, depth)
                 elif isinstance(obj, list):
                     self._rc_get_list(obj, memory, depth)
-                elif isinstance(obj, dict):
+                elif isinstance(obj, (dict, OrderedDict)):
                     self._rc_get_dict(obj, memory, depth)
 
             elif depth >= self.iterdepth:
@@ -34,6 +39,8 @@ class Attribute:
 
     def _rc_get_obj(self, obj, memory='', depth=0):
         if hasattr(obj, '__dict__') and len(obj.__dict__) != 0:
+            if not len(memory) : print(f'\n- LENGTH(obj) : {len(vars(obj))}')
+            else               : print(f'\n- LENGTH(obj~{memory}) : {len(vars(obj))}')
             ic = -1
             for key, attr in vars(obj).items():
                 if not self.itercount:
@@ -74,8 +81,8 @@ class Attribute:
 
     def _rc_get_list(self, obj, memory='', depth=0):
         if isinstance(obj, list):
-            if not len(memory) : print(f'- LENGTH(obj) : {len(obj)}')
-            else               : print(f'- LENGTH(obj~{memory}) : {len(obj)}')
+            if not len(memory) : print(f'\n- LENGTH(obj) : {len(obj)}')
+            else               : print(f'\n- LENGTH(obj~{memory}) : {len(obj)}')
             ic = -1
             for key, attr in enumerate(obj):
                 if not self.itercount:
@@ -117,8 +124,8 @@ class Attribute:
 
     def _rc_get_dict(self, obj, memory='', depth=0):
         if isinstance(obj, (dict, OrderedDict)):
-            if not len(memory) : print(f'- LENGTH(obj) : {len(obj)}')
-            else               : print(f'- LENGTH(obj~{memory}) : {len(obj)}')
+            if not len(memory) : print(f'\n- LENGTH(obj) : {len(obj)}')
+            else               : print(f'\n- LENGTH(obj~{memory}) : {len(obj)}')
             ic = -1
             for key, attr in obj.items():
                 if not self.itercount:
@@ -183,6 +190,120 @@ class Attribute:
         else:
             return None
 
+
+    def _rc_get_hdf5(self, obj, memory='', depth=0):
+        if isinstance(obj, (h5py._hl.files.File, h5py._hl.group.Group)):
+            if not len(memory) : print(f'\n- LENGTH(obj) : {len(obj.keys())}')
+            else               : print(f'\n- LENGTH(obj~{memory}) : {len(obj.keys())}')
+            ic = -1
+            for key in obj.keys():
+                if not self.itercount:
+                    if self.attrviewer:
+                        if not len(memory):
+                            if isinstance(key, str):
+                                print(f'["{key}"] : {obj[key]}')
+                            else:
+                                print(f'[{key}] : {obj[key]}')
+                        else:
+                            if isinstance(key, str):
+                                print(f'{memory}["{key}"] : {obj[key]}')
+                            else:    
+                                print(f'{memory}[{key}] : {obj[key]}')
+                    else:
+                        if not len(memory):
+                            if isinstance(key, str):
+                                print(f'["{key}"]')
+                            else:
+                                print(f'[{key}]')
+                        else:
+                            if isinstance(key, str):
+                                print(f'{memory}["{key}"]')
+                            else:    
+                                print(f'{memory}[{key}]')
+                
+                else:
+                    ic += 1
+                    if ic == self.itercount:
+                        break
+                    
+                    if self.attrviewer:
+                        if not len(memory):
+                            if isinstance(key, str):
+                                print(f'["{key}"] : {obj[key]}')
+                            else:
+                                print(f'[{key}] : {obj[key]}')
+                        else:
+                            if isinstance(key, str):
+                                print(f'{memory}["{key}"] : {obj[key]}')
+                            else:    
+                                print(f'{memory}[{key}] : {obj[key]}')
+                    else:
+                        if not len(memory):
+                            if isinstance(key, str):
+                                print(f'["{key}"]')
+                            else:
+                                print(f'[{key}]')
+                        else:
+                            if isinstance(key, str):
+                                print(f'{memory}["{key}"]')
+                            else:    
+                                print(f'{memory}[{key}]')
+
+
+            self.spliter(n=depth)
+            for key in obj.keys():
+                if isinstance(key, str):
+                    self.rc_get(obj[key], memory=memory+'["'+str(key)+'"]', depth=depth+1)
+                else:
+                    self.rc_get(obj[key], memory=memory+'['+str(key)+']', depth=depth+1)
+
+
+        elif isinstance(obj, h5py._hl.dataset.Dataset):
+            if not len(memory) : print(f'\n- LENGTH(obj) : {len(obj)}')
+            else               : print(f'\n- LENGTH(obj~{memory}) : {len(obj)}')
+            ic = -1
+            for key, attr in enumerate(obj):
+                if not self.itercount:
+                    if self.attrviewer:
+                        if not len(memory):
+                            print(f'[{key}] : {attr}')
+                        else:
+                            print(f'{memory}[{key}] : {attr}')
+                    else:
+                        if not len(memory):
+                            print(f'[{key}]')
+                        else:
+                            print(f'{memory}[{key}]')
+                
+                else:
+                    ic += 1
+                    if ic == self.itercount:
+                        break
+                    
+                    if self.attrviewer:
+                        if not len(memory):
+                            print(f'[{key}] : {attr}')
+                        else:
+                            print(f'{memory}[{key}] : {attr}')
+                    else:
+                        if not len(memory):
+                            print(f'[{key}]')
+                        else:
+                            print(f'{memory}[{key}]')
+
+
+            self.spliter(n=depth)
+            for key, attr in enumerate(obj):
+                if isinstance(key, str):
+                    self.rc_get(attr, memory=memory+'["'+str(key)+'"]', depth=depth+1)
+                else:
+                    self.rc_get(attr, memory=memory+'['+str(key)+']', depth=depth+1)
+
+        else:
+            return None
+
+        
+
     @staticmethod
     def spliter(n, turn=False):
         split_line = '-'*(100 - 5*n)
@@ -237,7 +358,7 @@ class logtrace:
                 print(f'===========================')
                 print(attribute.rc_get(obj))
 
-        print('\n* [2]-----------------------------------------------DETAILS INFO(method)-------------------------------------------*')
+        print('\n* [2]-------------------------------------------------DETAILS INFO(method)---------------------------------------------*')
 
         for key, values in logs.items():
             for i, obj in enumerate(values):
@@ -251,7 +372,7 @@ class logtrace:
         
 
 
-        print('\n* [3]-----------------------------------------------------FINAL LOG---------------------------------------------------*')
+        print('\n* [3]------------------------------------------------------FINAL LOG---------------------------------------------------*')
         
         for key, values in logs.items():
             for i, obj in enumerate(values):
@@ -302,10 +423,9 @@ class Debugger:
 
 
 def main():
-    dic = {1:1, 2:2, 3:3, 4:[1,2,3,4,5], 5:{'a':1, 'b':2, 'c':{3:3, 'a':1, 'b':2, 'ed':23}}}
-
-    debugger = Debugger(attrviewer=True, itercount=None, iterdepth=3)
-    debugger(dic, logname='here')
+    obj = {1:1, 2:2, 3:3, 4:[1,2,3,4,5], 5:{'a':1, 'b':2, 'c':{3:3, 'a':1, 'b':2, 'ed':23}}}
+    debugger = Debugger(attrviewer=True, itercount=3, iterdepth=3)
+    debugger(obj, logname='dic')
     del debugger
 
 
