2ae43cf342fa13cd68b74b971e91d3a87b952db4
userdky <userdyk@gmail.com>
2020-08-07 23:48:09 +0900

    project.tar.gz update

--- storage/project/execute.sh
+++ storage/project/execute.sh
@@ -2,6 +2,7 @@ id="ailever"
 CUDA_VISIBLE_DEVICES=0 python main.py --id $id \
 	--epochs 5 \
 	--batch_size 8 \
+	--dataset_path datasets/ \
 	--xlsx_path datasets/dataset.xlsx \
 	--json_path datasets/dataset.json \
 	--pkl_path datasets/dataset.pkl \
--- storage/project/options.py
+++ storage/project/options.py
@@ -23,18 +23,21 @@ def load():
                         default=8)
 
     # load_paths
+    parser.add_argument('--dataset_path',
+                        type=str,
+                        default='datasets/')
     parser.add_argument('--xlsx_path',
                         type=str,
-                        default='dataset/dataset.xlsx')
+                        default='datasets/dataset.xlsx')
     parser.add_argument('--json_path',
                         type=str,
-                        default='dataset/dataset.json')
+                        default='datasets/dataset.json')
     parser.add_argument('--pkl_path',
                         type=str,
-                        default='dataset/dataset.pkl')
+                        default='datasets/dataset.pkl')
     parser.add_argument('--hdf5_path',
                         type=str,
-                        default='dataset/dataset.hdf5')
+                        default='datasets/dataset.hdf5')
     
     options = parser.parse_args()
     return options
