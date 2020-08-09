fb382cc079efebdd7f22d94326311e9915f9b23c
userdky <userdyk@gmail.com>
2020-08-10 06:54:48 +0900

    update

--- ailever/utils/data/visualization.py
+++ ailever/utils/data/visualization.py
@@ -11,13 +11,14 @@ class Visualizer(Visdom):
                                          env='main')
         self._origin = 0
     
-    def originator_(self, feature_dim):
+    def originator_(self, f_dim, feature_dim):
         self._origin += 1
 
         setattr(self, f'window{self._origin}', obj())
-        setattr(self, f'window{self._origin}', self.line(Y=torch.Tensor(1, feature_dim).zero_(), opts=dict(title=f'METRIC{feature_dim}' if feature_dim == 1 else 'METRIC',
-                                                                                                   xlabel='Epoch',
-                                                                                                   ylabel=f'Scale{feature_dim}' if feature_dim == 1 else 'Scale')))
+        setattr(self, f'window{self._origin}', self.line(Y=torch.Tensor(1, feature_dim).zero_(), opts=dict(title=f'METRIC' if feature_dim > 1 else f'metric{f_dim+1}',
+                                                                                                   xlabel=f'EPOCH' if feature_dim > 1 else f'epoch',
+                                                                                                   ylabel=f'SCALE' if feature_dim > 1 else f'scale',
+                                                                                                   showlegend=True)))
         return getattr(self, f'window{self._origin}')
 
     
@@ -30,16 +31,16 @@ def visualizer(dataset):
     vis.close(env='main')
     
     # window generator
-    window = vis.originator_(feature_dim)
+    window = vis.originator_(None, feature_dim)
     if feature_dim > 1:
-        for i in range(feature_dim):
-            locals()[f'sub_window{i}'] = vis.originator_(1)
-        
+        for f_dim in range(feature_dim):
+            locals()[f'sub_window{f_dim}'] = vis.originator_(f_dim, 1)
+    
+    # line plot
     for i, data in enumerate(dataset):
         vis.line(X=torch.Tensor([[i]*feature_dim]), Y=data.unsqueeze(0), win=window, update='append', opts=dict(title='metrics'))
         
         if feature_dim > 1:
             for f_dim in range(feature_dim):
-                vis.line(X=torch.Tensor([[i]]), Y=data[f_dim].unsqueeze(0).unsqueeze(0), win=locals()[f'sub_window{f_dim}'], update='append', opts=dict(title=f'metric{f_dim}'))
-
+                vis.line(X=torch.Tensor([[i]]), Y=data[f_dim].unsqueeze(0).unsqueeze(0), win=locals()[f'sub_window{f_dim}'], update='append', opts=dict(title=f'metric{f_dim+1}'))
 
