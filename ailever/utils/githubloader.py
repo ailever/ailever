7fffd9f653460d36ffe5782295eb6d48f1ea7205
userdky <userdyk@gmail.com>
2020-08-13 04:16:38 +0900

    update

--- ailever/utils/githubloader.py
+++ ailever/utils/githubloader.py
@@ -3,12 +3,17 @@ import os
 def repository(repo):
     if repo == 'ailever':
         os.system('git clone https://github.com/ailever/ailever.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'programming-language':
         os.system('git clone https://github.com/ailever/programming-language.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'numerical-method':
         os.system('git clone https://github.com/ailever/numerical-method.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'applications':
         os.system('git clone https://github.com/ailever/applications.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'deep-learning':
         os.system('git clone https://github.com/ailever/deep-learning.git')
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
 
