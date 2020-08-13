c0a56c385e852d9049d93e298f5a7d5bdc0ce577
userdky <userdyk@gmail.com>
2020-08-14 07:54:20 +0900

    update

--- storage/experiment/datasets/_custom.py
+++ storage/experiment/datasets/_custom.py
@@ -7,12 +7,13 @@ obj = type('obj', (), {})
 class CustomDataset(Dataset):
     def __init__(self, options):
         self.options = options
-        
+        self.sizeofset = 10000
+
         if self.options.dataset_name == 'internal-univariate-linear-scalar':
             self.dataset = obj()
-            self.dataset.xy = torch.arange(0*1000*2, 3*1000*2).view(3*1000,2).type(torch.FloatTensor)
-            permutation = torch.randperm(3*1000)
-            spliter = permutation.split([1000,1000,1000])
+            self.dataset.xy = torch.arange(0*self.sizeofset*2, 3*self.sizeofset*2).view(3*self.sizeofset,2).type(torch.FloatTensor)
+            permutation = torch.randperm(3*self.sizeofset)
+            spliter = permutation.split([self.sizeofset,self.sizeofset,self.sizeofset])
 
             self.train_dataset = obj()
             self.train_dataset.x = self.dataset.xy[spliter[0]][:, 0:1]
@@ -28,25 +29,25 @@ class CustomDataset(Dataset):
         
         elif self.options.dataset_name == 'external-univariate-linear-scalar':
             self.train_dataset = obj()
-            self.train_dataset.xy = torch.arange(0*1000*2, 1*1000*2).view(1000,2).type(torch.FloatTensor)
+            self.train_dataset.xy = torch.arange(0*self.sizeofset*2, 1*self.sizeofset*2).view(self.sizeofset,2).type(torch.FloatTensor)
             self.train_dataset.x = self.train_dataset.xy[:, 0:1]
             self.train_dataset.y = self.train_dataset.xy[:, 1:2]
             
             self.validation_dataset = obj()
-            self.validation_dataset.xy = torch.arange(1*1000*2, 2*1000*2).view(1000,2).type(torch.FloatTensor)
+            self.validation_dataset.xy = torch.arange(1*self.sizeofset*2, 2*self.sizeofset*2).view(self.sizeofset,2).type(torch.FloatTensor)
             self.validation_dataset.x = self.validation_dataset.xy[:, 0:1]
             self.validation_dataset.y = self.validation_dataset.xy[:, 1:2]
 
             self.test_dataset = obj()
-            self.test_dataset.xy = torch.arange(2*1000*2, 3*1000*2).view(1000,2).type(torch.FloatTensor)
+            self.test_dataset.xy = torch.arange(2*self.sizeofset*2, 3*self.sizeofset*2).view(self.sizeofset,2).type(torch.FloatTensor)
             self.test_dataset.x = self.test_dataset.xy[:, 0:1]
             self.test_dataset.y = self.test_dataset.xy[:, 1:2]
 
         elif self.options.dataset_name == 'internal-multivariate-linear-scalar':
             self.dataset = obj()
-            self.dataset.xy = torch.arange(0*1000*10, 3*1000*10).view(3*1000,10).type(torch.FloatTensor)
-            permutation = torch.randperm(3*1000)
-            spliter = permutation.split([1000,1000,1000])
+            self.dataset.xy = torch.arange(0*self.sizeofset*10, 3*self.sizeofset*10).view(3*self.sizeofset,10).type(torch.FloatTensor)
+            permutation = torch.randperm(3*self.sizeofset)
+            spliter = permutation.split([self.sizeofset,self.sizeofset,self.sizeofset])
 
             self.train_dataset = obj()
             self.train_dataset.x = self.dataset.xy[spliter[0]][:, 0:9]
@@ -62,25 +63,25 @@ class CustomDataset(Dataset):
         
         elif self.options.dataset_name == 'external-multivariate-linear-scalar':
             self.train_dataset = obj()
-            self.train_dataset.xy = torch.arange(0*1000*10, 1*1000*10).view(1000,10).type(torch.FloatTensor)
+            self.train_dataset.xy = torch.arange(0*self.sizeofset*10, 1*self.sizeofset*10).view(self.sizeofset,10).type(torch.FloatTensor)
             self.train_dataset.x = self.train_dataset.xy[:, 0:9]
             self.train_dataset.y = self.train_dataset.xy[:, 9:10]
             
             self.validation_dataset = obj()
-            self.validation_dataset.xy = torch.arange(1*1000*10, 2*1000*10).view(1000,10).type(torch.FloatTensor)
+            self.validation_dataset.xy = torch.arange(1*self.sizeofset*10, 2*self.sizeofset*10).view(self.sizeofset,10).type(torch.FloatTensor)
             self.validation_dataset.x = self.validation_dataset.xy[:, 0:9]
             self.validation_dataset.y = self.validation_dataset.xy[:, 9:10]
 
             self.test_dataset = obj()
-            self.test_dataset.xy = torch.arange(2*1000*10, 3*1000*10).view(1000,10).type(torch.FloatTensor)
+            self.test_dataset.xy = torch.arange(2*self.sizeofset*10, 3*self.sizeofset*10).view(self.sizeofset,10).type(torch.FloatTensor)
             self.test_dataset.x = self.test_dataset.xy[:, 0:9]
             self.test_dataset.y = self.test_dataset.xy[:, 9:10]
 
         elif self.options.dataset_name == 'internal-multivariate-linear-vector':
             self.dataset = obj()
-            self.dataset.xy = torch.arange(0*1000*10, 3*1000*10).view(3*1000,10).type(torch.FloatTensor)
-            permutation = torch.randperm(3*1000)
-            spliter = permutation.split([1000,1000,1000])
+            self.dataset.xy = torch.arange(0*self.sizeofset*10, 3*self.sizeofset*10).view(3*self.sizeofset,10).type(torch.FloatTensor)
+            permutation = torch.randperm(3*self.sizeofset)
+            spliter = permutation.split([self.sizeofset,self.sizeofset,self.sizeofset])
 
             self.train_dataset = obj()
             self.train_dataset.x = self.dataset.xy[spliter[0]][:, 0:7]
@@ -96,17 +97,17 @@ class CustomDataset(Dataset):
 
         elif self.options.dataset_name == 'external-multivariate-linear-vector':
             self.train_dataset = obj()
-            self.train_dataset.xy = torch.arange(0*1000*10, 1*1000*10).view(1000,10).type(torch.FloatTensor)
+            self.train_dataset.xy = torch.arange(0*self.sizeofset*10, 1*self.sizeofset*10).view(self.sizeofset,10).type(torch.FloatTensor)
             self.train_dataset.x = self.train_dataset.xy[:, 0:7]
             self.train_dataset.y = self.train_dataset.xy[:, 7:10]
             
             self.validation_dataset = obj()
-            self.validation_dataset.xy = torch.arange(1*1000*10, 2*1000*10).view(1000,10).type(torch.FloatTensor)
+            self.validation_dataset.xy = torch.arange(1*self.sizeofset*10, 2*self.sizeofset*10).view(self.sizeofset,10).type(torch.FloatTensor)
             self.validation_dataset.x = self.validation_dataset.xy[:, 0:7]
             self.validation_dataset.y = self.validation_dataset.xy[:, 7:10]
 
             self.test_dataset = obj()
-            self.test_dataset.xy = torch.arange(2*1000*10, 3*1000*10).view(1000,10).type(torch.FloatTensor)
+            self.test_dataset.xy = torch.arange(2*self.sizeofset*10, 3*self.sizeofset*10).view(self.sizeofset,10).type(torch.FloatTensor)
             self.test_dataset.x = self.test_dataset.xy[:, 0:7]
             self.test_dataset.y = self.test_dataset.xy[:, 7:10]
 
