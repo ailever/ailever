266b607248f683eb126356d7b170d3d5d8547829
userdky <userdyk@gmail.com>
2020-08-30 21:18:16 +0900

    update

--- ailever/utils/loader.py
+++ ailever/utils/loader.py
@@ -9,26 +9,26 @@ def storage(name):
 def repository(repo):
     if repo == 'ailever':
         os.system('git clone https://github.com/ailever/ailever.git')
-        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'programming-language':
         os.system('git clone https://github.com/ailever/programming-language.git')
-        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'numerical-method':
         os.system('git clone https://github.com/ailever/numerical-method.git')
-        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'statistics':
         os.system('git clone https://github.com/ailever/statistics.git')
-        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'applications':
         os.system('git clone https://github.com/ailever/applications.git')
-        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
     elif repo == 'deep-learning':
         os.system('git clone https://github.com/ailever/deep-learning.git')
-        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    
+    print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
 
 
 def cloud(name=None):
-    urlretrieve('https://docs.google.com/uc?export=download&id=1MFQuoE1B58TSice5aPDqQDdSJqu1CLB3', f'./template.pptx')
-    print(f'[AILEVER] The file "template.pptx" is downloaded!')
+    if name == 'template.pptx':
+        urlretrieve('https://docs.google.com/uc?export=download&id=1MFQuoE1B58TSice5aPDqQDdSJqu1CLB3', f'./{name}')
+    elif name == 'regression.pptx':
+        urlretrieve('https://docs.google.com/uc?export=download&id=1OsS7Dd56yf9VftF7BBLWxECI9l_xmksG', f'./{name}')
+
+    print(f'[AILEVER] The file "{name}" is downloaded!')
 
 
