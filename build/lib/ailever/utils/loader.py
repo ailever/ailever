53515abb608273ff39a72aed69975a41ea837c96
userdky <userdyk@gmail.com>
2020-09-03 19:58:14 +0900

    update

--- build/lib/ailever/utils/loader.py
+++ build/lib/ailever/utils/loader.py
@@ -2,12 +2,31 @@ import os
 from urllib.request import urlretrieve
 
 def storage(name):
+    if name == 'list':
+        print('[AILEVER] List of contents in the storage')
+        contents = ['experiment.tar.gz', 'experiment.zip',
+                    'template.tar.gz', 'template.zip']
+        for content in contents:
+            print(f'* {content}')
+    
     urlretrieve('https://raw.githubusercontent.com/ailever/ailever/master/storage/'+name, f'./{name}')
-    print(f'[AILEVER] The file "{name}" is downloaded!')
+    if name != 'list':
+        print(f'[AILEVER] The file "{name}" is downloaded!')
 
 
 def repository(repo):
-    if repo == 'ailever':
+    if repo == 'list':
+        print('[AILEVER] List of contents in the repository')
+        contents = ['ailever',
+                    'programming-language',
+                    'numerical-method',
+                    'statistics',
+                    'applications',
+                    'deep-learning']
+        for content in contents:
+            print(f'* {content}')
+
+    elif repo == 'ailever':
         os.system('git clone https://github.com/ailever/ailever.git')
     elif repo == 'programming-language':
         os.system('git clone https://github.com/ailever/programming-language.git')
@@ -20,15 +39,24 @@ def repository(repo):
     elif repo == 'deep-learning':
         os.system('git clone https://github.com/ailever/deep-learning.git')
     
-    print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
+    if repo != 'list':
+        print(f'[AILEVER] The repository "{repo}" is successfully cloned!')
 
 
 def cloud(name=None):
-    if name == 'template.pptx':
+    if name == 'list':
+        print('[AILEVER] List of contents in the cloud')
+        contents = ['template.pptx',
+                    'regression.pptx']
+        for content in contents:
+            print(f'* {content}')
+
+    elif name == 'template.pptx':
         urlretrieve('https://docs.google.com/uc?export=download&id=1MFQuoE1B58TSice5aPDqQDdSJqu1CLB3', f'./{name}')
     elif name == 'regression.pptx':
         urlretrieve('https://docs.google.com/uc?export=download&id=1OsS7Dd56yf9VftF7BBLWxECI9l_xmksG', f'./{name}')
-
-    print(f'[AILEVER] The file "{name}" is downloaded!')
+    
+    if name != 'list':
+        print(f'[AILEVER] The file "{name}" is downloaded!')
 
 
