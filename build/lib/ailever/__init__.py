f7a9c4a52a13991d52c935710946960cf1d18ab3
userdky <userdyk@gmail.com>
2020-09-18 18:00:13 +0900

    update

--- build/lib/ailever/__init__.py
+++ build/lib/ailever/__init__.py
@@ -3,12 +3,10 @@ from .security import SecurityError, sudo
 
 account = input('[AILEVER] enter your ID : ')
 if account in sudo.members():
-    supervisor_id = input(f'[AILEVER] your account "{account}" was succesfully inspected.')
+    supervisor_id = input(f'[AILEVER] your account "{account}" was succesfully inspected. Press Enter.')
     passwd = input(f'[AILEVER] enter password : ')
     if sudo.identify(account, passwd):
         supervisor_passwd = input(f'[AILEVER] your account "{account}" was succesfully. Welcome to Ailever!')
-        if sudo.identify(supervisor_id, supervisor_passwd):
-            pass
     else : raise SecurityError('[AILEVER] your password is incorrect.')
 else : raise SecurityError('[AILEVER] you are not a member of Ailever.')
 
