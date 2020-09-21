9c87eff0636cd848047fe9e2034b98a0c044116b
userdky <userdyk@gmail.com>
2020-09-22 04:25:30 +0900

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name                = 'ailever',
-      version             = '0.0.62',
+      version             = '0.0.63',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
