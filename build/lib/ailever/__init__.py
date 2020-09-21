976b3e618ed4bdbbd388ebca68dacc6504d7653e
userdky <userdyk@gmail.com>
2020-09-22 04:10:24 +0900

    update

--- build/lib/ailever/__init__.py
+++ build/lib/ailever/__init__.py
@@ -7,8 +7,9 @@ account = input('[AILEVER] Enter your ID : ')
 if account in sudo.members():
     passwd = input(f'[AILEVER] Enter password : ')
     if sudo.identify(account, passwd):
-        supervisor_id = input(f'Your account was succesfully logged-in.')
-        supervisor_passwd = input(f' Welcome to Ailever! Promulgate values for a better tomorrow!')
+        pass
+        #supervisor_id = input(f'Your account was succesfully logged-in.')
+        #supervisor_passwd = input(f' Welcome to Ailever! Promulgate values for a better tomorrow!')
     else : raise SecurityError('[AILEVER] Your password is incorrect.')
 else : raise SecurityError('[AILEVER] You are not a member of Ailever.')
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name                = 'ailever',
-      version             = '0.0.60',
+      version             = '0.0.61',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
