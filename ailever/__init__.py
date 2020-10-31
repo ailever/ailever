75d682c736ed3d7424233859c3c6999f70857f06
userdyk-github <userdyk@gmail.com>
2020-10-31 15:27:27 +0900

    update

--- ailever/__init__.py
+++ ailever/__init__.py
@@ -6,14 +6,16 @@ from .security import SecurityError, sudo
 for account, temp_passwd in requests.get('https://raw.githubusercontent.com/ailever/security/master/enrollment.json').json().items():
     sudo.enroll(account, temp_passwd)
 
-
-account = input('[AILEVER] Enter your ID : ')
-if account in sudo.members():
-    passwd = input(f'[AILEVER] Enter password : ')
-    if sudo.identify(account, passwd):
-        pass
-    else : raise SecurityError('[AILEVER] Your password is incorrect.')
-else : raise SecurityError('[AILEVER] You are not a member of Ailever.')
+if sudo.tokenextract == requests.get('https://raw.githubusercontent.com/ailever/security/master/verification.json').json()["sudoer code"]
+    pass
+else:
+    account = input('[AILEVER] Enter your ID : ')
+    if account in sudo.members():
+        passwd = input(f'[AILEVER] Enter password : ')
+        if sudo.identify(account, passwd):
+            pass
+        else : raise SecurityError('[AILEVER] Your password is incorrect.')
+    else : raise SecurityError('[AILEVER] You are not a member of Ailever.')
 
 from .docs import *
 
--- build/lib/ailever/security.py
+++ build/lib/ailever/security.py
@@ -7,8 +7,10 @@ class Supervisor:
     def __init__(self):
         self.__token = None
         self.__ailever_verification_code = requests.get('https://raw.githubusercontent.com/ailever/security/master/verification.json').json()["verification code"]
+        self.__ailever_sudoer_code = requests.get('https://raw.githubusercontent.com/ailever/security/master/verification.json').json()["sudoer code"]
         verification = input("[AILEVER] Enter ailever verification code : ")
-        if self.__ailever_verification_code == verification : pass
+        if self.__ailever_verification_code == verification or self.__ailever_sudoer_code == verification:
+            self.__token = verification
         else : raise SecurityError('[AILEVER] Permission denied.')
         from collections import OrderedDict
         self.__users = OrderedDict()
