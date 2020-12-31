4ebbf2e3c3dc96cac18143045861e28dfabc4141
Dongmyeong Lee <52376448+userdyk-github@users.noreply.github.com>
2020-12-31 15:47:03 +0900

    Update P1T1.py

--- ailever/apps/AppBRAIN/P1T1.py
+++ ailever/apps/AppBRAIN/P1T1.py
@@ -1,4 +1,17 @@
+import plotly.express as px
+
 class Components(dict):
     def __init__(self):
-        pass
+        self['0,0'] = None
+        self.updateR0C0()
+
+    def updateR0C0(self):
+        df = px.data.iris()
+        self['0,0'] = px.parallel_coordinates(df, color="species_id", labels={"species_id": "Species",
+                "sepal_width": "Sepal Width", "sepal_length": "Sepal Length",
+                "petal_width": "Petal Width", "petal_length": "Petal Length", },
+                             color_continuous_scale=px.colors.diverging.Tealrose,
+                             color_continuous_midpoint=2)
+
+
 
