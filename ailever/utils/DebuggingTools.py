e21bfdd97a2ab895a0ecdbb3bde5fb809da75bcb
userdyk-github <userdyk@gmail.com>
2021-01-06 15:45:43 +0900

    update

---
+++ ailever/utils/DebuggingTools.py
@@ -0,0 +1,15 @@
+
+class _Debug:
+    def __call__(self, attrviewer=False, itercount=None, iterdepth=None):
+        from .debug import Debug
+        debug = Debug(attrviewer, itercount, iterdepth)
+        return debug
+
+class _Torchbug:
+    def __call__(self):
+        from .torchbug import Torchbug
+        torchbug = Torchbug()
+        return torchbug
+
+Debug = _Debug()
+Touchbug = _Touchbug()
