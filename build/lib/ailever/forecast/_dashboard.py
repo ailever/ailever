ae3025f0a04b3c57d86718882656f4bb4e4fd2f0
userdyk-github <userdyk@gmail.com>
2021-01-13 14:35:29 +0900

    update

--- build/lib/ailever/forecast/_dashboard.py
+++ build/lib/ailever/forecast/_dashboard.py
@@ -1,7 +1,9 @@
 import os
 from urllib.request import urlretrieve
 
-def dashboard(name):
-    urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/forecast/'+name+'.py', f'./{name}.py')
-    print(f'[AILEVER] The file "{name}.py" is downloaded!')
+def dashboard(name, host='127.0.0.1', port='8050'):
+    if not os.path.isfile(f'{name}.py'):
+        urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/forecast/'+name+'.py', f'./{name}.py')
+        print(f'[AILEVER] The file "{name}.py" is downloaded!')
+    os.system(f'python {name}.py --ds {host} --dp {port}')
 
