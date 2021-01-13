4ea5bc17a889ae2f03f7640a088b960f5e4c1b8f
userdyk-github <userdyk@gmail.com>
2021-01-13 13:14:55 +0900

    update

---
+++ ailever/forecast/_dashboard.py
@@ -0,0 +1,7 @@
+import os
+from urllib.request import urlretrieve
+
+def dashboard(name):
+    urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/forecast/'+name+'.py', f'./{name}.py')
+    print(f'[AILEVER] The file "{name}.py" is downloaded!')
+
