b6e3618bf4d8a3c83c9d9238fa36d1ba10486af2
userdyk-github <userdyk@gmail.com>
2021-01-15 11:33:12 +0900

    update

--- ailever/forecast/stock/krx.py
+++ ailever/forecast/stock/krx.py
@@ -1,64 +1,100 @@
+import os
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
 import FinanceDataReader as fdr
 
-def all(date='2010-01-01', mode='Close'):
+
+def download():
+    if not os.path.isdir('stockset'):
+        os.mkdir('stockset')
+        fdr.StockListing('KRX').to_csv('stockset/KRX.csv')
+        symbols = pd.read_csv('stockset/KRX.csv').Symbol.values
+
+        for symbol in tqdm(symbols):
+            fdr.DataReader(symbol).to_csv(f'stockset/{symbol}.csv')
+
+
+
+def _all(date='2010-01-01', mode='Close', cut=None):
+    date = np.datetime64(date)
     stock_list = fdr.StockListing('KRX')
-    stock_list = stock_list[stock_list.ListingDate <= np.datetime64(date)]
-    symbols = stock_list.Symbol
+    symbols = stock_list.Symbol.values
+    stocks = fdr.DataReader('005930', date)
+    stocks = stocks[f'{mode}'].values
 
-    for i, symbol in enumerate(symbols):
-        if i == 0:
-            stocks = fdr.DataReader(symbol)[f'{mode}'].values
-        else:
-            stock = fdr.DataReader(symbol)[f'{mode}'].values
+    exception_list = list()
+    for i, symbol in enumerate(tqdm(symbols)):
+        if i == cut: break
+        try:
+            stock = fdr.DataReader(symbol, date)
+            stock = stock[f'{mode}'].values
             stocks = np.c_[stocks, stock]
+        except:
+            exception_list.append(symbol)
+        
+    return stocks[:, 1:], stock_list, exception_list
 
-    return stocks, stock_list
 
-
-def kospi(date='2010-01-01', mode='Close'):
+def _kospi(date='2010-01-01', mode='Close', cut=None):
+    date = np.datetime64(date)
     stock_list = fdr.StockListing('KRX')
     stock_list = stock_list[stock_list.Market == 'KOSPI']
-    stock_list = stock_list[stock_list.ListingDate <= np.datetime64(date)]
-    symbols = stock_list.Symbol
+    symbols = stock_list.Symbol.values
+    stocks = fdr.DataReader('005930', date)
+    stocks = stocks[f'{mode}'].values
 
-    for i, symbol in enumerate(symbols):
-        if i == 0:
-            stocks = fdr.DataReader(symbol)[f'{mode}'].values
-        else:
-            stock = fdr.DataReader(symbol)[f'{mode}'].values
+    exception_list = list()
+    for i, symbol in enumerate(tqdm(symbols)):
+        if i == cut: break
+        try:
+            stock = fdr.DataReader(symbol, date)
+            stock = stock[f'{mode}'].values
             stocks = np.c_[stocks, stock]
+        except:
+            exception_list.append(symbol)
 
-    return stocks, stock_list
+    return stocks[:, 1:], stock_list, exception_list
 
 
-def kosdaq(date='2010-01-01', mode='Close'):
+def _kosdaq(date='2010-01-01', mode='Close', cut=None):
+    date = np.datetime64(date)
     stock_list = fdr.StockListing('KRX')
     stock_list = stock_list[stock_list.Market == 'KOSDAQ']
-    stock_list = stock_list[stock_list.ListingDate <= np.datetime64(date)]
-    symbols = stock_list.Symbol
+    symbols = stock_list.Symbol.values
+    stocks = fdr.DataReader('005930', date)
+    stocks = stocks[f'{mode}'].values
 
-    for i, symbol in enumerate(symbols):
-        if i == 0:
-            stocks = fdr.DataReader(symbol)[f'{mode}'].values
-        else:
-            stock = fdr.DataReader(symbol)[f'{mode}'].values
+    exception_list = list()
+    for i, symbol in enumerate(tqdm(symbols)):
+        if i == cut: break
+        try:
+            stock = fdr.DataReader(symbol, date)
+            stock = stock[f'{mode}'].values
             stocks = np.c_[stocks, stock]
+        except:
+            exception_list.append(symbol)
 
-    return stocks, stock_list
+    return stocks[:, 1:], stock_list, exception_list
 
 
-def konex(date='2010-01-01', mode='Close'):
+def _konex(date='2010-01-01', mode='Close', cut=None):
+    date = np.datetime64(date)
     stock_list = fdr.StockListing('KRX')
     stock_list = stock_list[stock_list.Market == 'KONEX']
-    stock_list = stock_list[stock_list.ListingDate <= np.datetime64(date)]
-    symbols = stock_list.Symbol
+    symbols = stock_list.Symbol.values
+    stocks = fdr.DataReader('005930', date)
+    stocks = stocks[f'{mode}'].values
 
-    for i, symbol in enumerate(symbols):
-        if i == 0:
-            stocks = fdr.DataReader(symbol)[f'{mode}'].values
-        else:
-            stock = fdr.DataReader(symbol)[f'{mode}'].values
+    exception_list = list()
+    for i, symbol in enumerate(tqdm(symbols)):
+        if i == cut: break
+        try:
+            stock = fdr.DataReader(symbol, date)
+            stock = stock[f'{mode}'].values
             stocks = np.c_[stocks, stock]
+        except:
+            exception_list.append(symbol)
 
-    return stocks, stock_list
+    return stocks[:, 1:], stock_list, exception_list
+
