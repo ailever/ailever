8d8fbc47a727a48e0b35865adb8ff4bc59f70351
userdyk-github <userdyk@gmail.com>
2021-01-17 18:23:39 +0900

    update

--- build/lib/ailever/_dashboard.py
+++ build/lib/ailever/_dashboard.py
@@ -5,8 +5,8 @@ import time
 
 queue = Queue()
 
-def remover(name, queue=queue):
-    time.sleep(20)
+def remover(name, sustain=20, queue=queue):
+    time.sleep(sustain)
     os.remove(f'./{name}.py')
     while not queue.empty():
         os.kill(queue.get(), signal.SIGTERM)
@@ -15,8 +15,8 @@ def _dashboard(name, host='127.0.0.1', port='8050', queue=queue):
     queue.put(os.getpid())
     os.system(f'python {name}.py --ds {host} --dp {port}')
 
-def dashboard(name, host='127.0.0.1', port='8050', queue=queue):
-    proc1 = Process(target=remover, args=(name, queue, ))
+def dashboard(name, host='127.0.0.1', port='8050', sustain=20, queue=queue):
+    proc1 = Process(target=remover, args=(name, sustain, queue, ))
     proc2 = Process(target=_dashboard, args=(name, host, port, queue, ))
     proc1.start()
     proc2.start()
