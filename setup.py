a49835439b177ecbc368cf43faea9e6cf6a43ea2
userdyk-github <userdyk@gmail.com>
2021-01-23 17:08:12 +0900

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name                = 'ailever',
-      version             = '0.1.120',
+      version             = '0.1.121',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
