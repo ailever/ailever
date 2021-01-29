4be11e12c4be417409dabcafc2537b1c8d49741b
userdyk-github <userdyk@gmail.com>
2021-01-30 01:18:47 +0900

    update

--- distribution.sh
+++ distribution.sh
@@ -1,5 +1,5 @@
 python setup.py bdist_wheel
-python -m twine upload dist/ailever-0.1.$1-py3-none-any.whl
+python -m twine upload dist/ailever-0.2.$1-py3-none-any.whl
 git add .
 git commit -m "update"
 git push
