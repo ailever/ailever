5729fa067e0518cf8f0e7916ec3534bee79ce04d
userdyk-github <userdyk@gmail.com>
2021-02-05 18:15:57 +0900

    update

--- ailever/forecast/_typecore_f.py
+++ ailever/forecast/_typecore_f.py
@@ -5,11 +5,6 @@ import torch
 
 def ForecastTypeCaster(obj, outtype='array'):
     obj = BaseTypeCaster(obj, outtype='BTC')
-
-    obj.list = [1,2,3]
-    obj.array = np.array([1,2,3])
-    obj.tensor = torch.tensor([1,2,3])
-    obj.frame = pd.DataFrame([1,2,3])
     _TypeCoreSave(obj, module_path=['ailever', 'forecast'], DS='AIL_DS_Forecast.pkl')
     obj = _TypeCoreLoad(module_path=['ailever', 'forecast'], DS='AIL_DS_Forecast.pkl')
     obj = getattr(obj, outtype)
