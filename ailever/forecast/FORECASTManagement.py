d8878f37fce8323fcc1554b223a398e3dd446bde
userdyk-github <userdyk@gmail.com>
2021-02-09 17:09:57 +0900

    update

--- ailever/forecast/FORECASTManagement.py
+++ ailever/forecast/FORECASTManagement.py
@@ -1,14 +1,14 @@
 def dashboard(name, host='127.0.0.1', port='8050'):
     from ._dashboard import dashboard
-    dashboard(name, host='127.0.0.1', port='8050')
+    dashboard(name=name, host=host, port=port)
 
 def regressor(norm):
     from ._stattools import regressor
-    return regressor(norm)
+    return regressor(norm=norm)
 
 def TSA(TS, lag=1, select_col=0, visualize=True):
     from ._tsa import TSA
-    return TSA(TS, lag=1, select_col=0, visualize=True)
+    return TSA(TS=TS, lag=lag, select_col=select_col, visualize=visualize)
 
 def RSDA():
     from .ts_residual import RSDA
