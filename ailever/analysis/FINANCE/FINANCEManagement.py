30d6341efe9884786884720c0d2a6f0d09d31a69
userdyk-github <userdyk@gmail.com>
2021-02-28 14:41:39 +0900

    update

---
+++ ailever/analysis/FINANCE/FINANCEManagement.py
@@ -0,0 +1,4 @@
+def InfluenceDiagram(labels, source, target, value, title="Influence Diagram", returnTrue=False):
+    from ._influence_diagram import InfluenceDiagram
+    InfluenceDiagram(labels=labels, source=source, target=target, value=value, title=title, returnTrue=returnTrue)
+
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
  
 setup(name                = 'ailever',
-      version             = '0.2.193',
+      version             = '0.2.194',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
