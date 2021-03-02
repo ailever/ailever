5728b4d03c74d3c38b9255836114bd40697feca0
userdyk-github <userdyk@gmail.com>
2021-03-02 17:45:00 +0900

    update

--- ailever/utils/VISUAL/_hbar.py
+++ ailever/utils/VISUAL/_hbar.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 
-def visualize(LEVELs, returnTrue=False):
+def visualize(LEVELs, title=title, returnTrue=False):
     r"""
     Example:
         >>> from ailever.utils import VISUAL
