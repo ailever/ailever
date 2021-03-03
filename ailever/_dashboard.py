b951065fae32d3932be001e6f6ce3b21f7378cee
userdyk-github <userdyk@gmail.com>
2021-03-04 08:36:39 +0900

    update

--- ailever/_dashboard.py
+++ ailever/_dashboard.py
@@ -18,7 +18,7 @@ def _dashboard_D(name, host='127.0.0.1', port='8050', queue=queue):
 
 def _dashboard(name, host='127.0.0.1', port='8050'):
     if not os.path.isfile(f'{name}.py'):
-        urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/{name}.py', f'./{name}.py')
+        urlretrieve(f'https://raw.githubusercontent.com/ailever/openapi/master/{name}.py', f'./{name}.py')
         print(f'[AILEVER] The file "{name}.py" is downloaded!')
     os.system(f'python {name}.py --ds {host} --dp {port}')
 
