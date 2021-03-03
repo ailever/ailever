d47d0556dcb301bccfb0d94fe8ce60499dc0f006
userdyk-github <userdyk@gmail.com>
2021-03-04 08:29:22 +0900

    update

--- build/lib/ailever/_dashboard.py
+++ build/lib/ailever/_dashboard.py
@@ -7,23 +7,28 @@ import time
 queue = Queue()
 
 def remover(name, sustain, queue=queue):
-    if sustain:
-        time.sleep(sustain)
-        os.remove(f'./{name}.py')
-        while not queue.empty():
-            os.kill(queue.get(), signal.SIGTERM)
+    time.sleep(sustain)
+    os.remove(f'./{name}.py')
+    while not queue.empty():
+        os.kill(queue.get(), signal.SIGTERM)
 
-def _dashboard(name, host='127.0.0.1', port='8050', queue=queue):
-    if not os.path.isfile(f'{name}.py'):
-        urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/main.py', f'./main.py')
-        print(f'[AILEVER] The file "{name}.py" is downloaded!')
+def _dashboard_D(name, host='127.0.0.1', port='8050', queue=queue):
     queue.put(os.getpid())
     os.system(f'python {name}.py --ds {host} --dp {port}')
 
-def dashboard(name, host='127.0.0.1', port='8050', sustain=None, queue=queue):
-    proc1 = Process(target=remover, args=(name, sustain, queue, ))
-    proc2 = Process(target=_dashboard, args=(name, host, port, queue, ))
-    proc1.start()
-    proc2.start()
-    proc1.join()
-    proc2.join()
+def _dashboard(name, host='127.0.0.1', port='8050'):
+    if not os.path.isfile(f'{name}.py'):
+        urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/{name}.py', f'./{name}.py')
+        print(f'[AILEVER] The file "{name}.py" is downloaded!')
+    os.system(f'python {name}.py --ds {host} --dp {port}')
+
+def dashboard(name='main', host='127.0.0.1', port='8050', sustain=20, queue=queue, D=False):
+    if D:
+        proc1 = Process(target=remover, args=(name, sustain, queue, ))
+        proc2 = Process(target=_dashboard_D, args=(name, host, port, queue, ))
+        proc1.start()
+        proc2.start()
+        proc1.join()
+        proc2.join()
+    else:
+        _dashboard(name=name, host=host, port=port)
