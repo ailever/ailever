864816ff3b9058e1aab653cffa66e55dbe5104c0
userdyk-github <userdyk@gmail.com>
2021-03-04 14:44:24 +0900

    update

--- build/lib/ailever/_dashboard.py
+++ build/lib/ailever/_dashboard.py
@@ -2,8 +2,13 @@ import os
 from urllib.request import urlretrieve
 
 def dashboard(name='main', host='127.0.0.1', port='8050'):
-    if not os.path.isfile(f'{name}.py'):
-        urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/'+name+'.py', f'./{name}.py')
-        print(f'[AILEVER] The file "{name}.py" is downloaded!')
-    os.system(f'python {name}.py --ds {host} --dp {port}')
+    if name=='main':
+        if not os.path.isfile(f'{name}.py'):
+            urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/'+name+'.py', f'./{name}.py')
+            print(f'[AILEVER] The file "{name}.py" is downloaded!')
+    else:
+        if not os.path.isfile(f'{name}.py'):
+            urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/work-sheet/'+name+'.py', f'./{name}.py')
+            print(f'[AILEVER] The file "{name}.py" is downloaded!')
 
+    os.system(f'python {name}.py --ds {host} --dp {port}')
