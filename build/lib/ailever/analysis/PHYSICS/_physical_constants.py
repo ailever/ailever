79f389ed4b6f41caeccd2fe56721c5e7ff9a0a6f
userdyk-github <userdyk@gmail.com>
2021-04-22 13:40:54 +0900

    update

---
+++ build/lib/ailever/analysis/PHYSICS/_physical_constants.py
@@ -0,0 +1,6 @@
+physical_constants = dict()
+physical_constants['reduced planck constant'] = 1.054571
+physical_constants['planck constant'] = 6.626070
+physical_constants['vacuum electric permittivity'] = 8.854187
+physical_constants['elementary charge'] = 1.602176
+physical_constants['avogadro constant'] = 6.022140
