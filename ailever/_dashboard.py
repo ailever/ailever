215ba3f13217fce6af370698bbc75df698765a7d
userdyk-github <userdyk@gmail.com>
2021-05-14 15:56:14 +0900

    update

--- ailever/_dashboard.py
+++ ailever/_dashboard.py
@@ -1,20 +1,48 @@
 import os
+import argparse
 from urllib.request import urlretrieve
+parser = argparse.ArgumentParser(description="dashboard parser")
+parser.add_argument('--HostDash', type=str, required=False, default='PassToken', help="host : dashboard")
+parser.add_argument('--PortDash', type=str, required=False, default='PassToken', help="port : dashboard")
+parser.add_argument('--HostDB', type=str, required=False, default='PassToken', help="host : database")
+parser.add_argument('--PortDB', type=str, required=False, default='PassToken', help="port : database")
+parser.add_argument('--HostRV', type=str, required=False, default='PassToken', help="host : real-time visualization")
+parser.add_argument('--PortRV', type=str, required=False, default='PassToken', help="port : real-time visualization")
+parser.add_argument('--HostR', type=str, required=False, default='PassToken', help="host : language r")
+parser.add_argument('--PortR', type=str, required=False, default='PassToken', help="port : language r")
+args = parser.parse_args()
 
-def dashboard(name='main', host='127.0.0.1', port='8050'):
+def dashboard(name='main',
+              HostDash=args.HostDash,
+              PortDash=args.PortDash,
+              HostDB=args.HostDB,
+              PortDB=args.PortDB,
+              HOSTRV=args.HostRV,
+              PortRV=args.PortRV,
+              HostR=args.HostR,
+              PortR=args.PortR,
+              ):
     if name == 'main':
         if not os.path.isfile(f'{name}.py'):
             urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/'+name+'.py', f'./{name}.py')
-            print(f'[AILEVER] The file "{name}.py" is downloaded!')
+            print(f'[AILEVER] The file "{name}.py" has been sucessfully downloaded!')
     elif name[:2] == 'WS':
         if not os.path.isfile(f'{name}.py'):
             urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/work-sheet/'+name+'.py', f'./{name}.py')
-            print(f'[AILEVER] The file "{name}.py" is downloaded!')
+            print(f'[AILEVER] The file "{name}.py" has been sucessfully downloaded!')
     elif name[:4] == 'PROJ':
         if not os.path.isfile(f'{name}.py'):
             urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/project/'+name+'.py', f'./{name}.py')
-            print(f'[AILEVER] The file "{name}.py" is downloaded!')
+            print(f'[AILEVER] The file "{name}.py" has been sucessfully downloaded!')
     else:
-        print(f'[AILEVER] Download is failed.')
+        raise Exception("[AILEVER] Download is failed.")
 
-    os.system(f'python {name}.py --ds {host} --dp {port}')
+    os.system(f'python {name}.py \
+            --HostDash {HostDash} \
+            --PortDash {PortDash} \
+            --HostDB {HostDB} \
+            --PortDB {PortDB} \
+            --HostRV {HostRV} \
+            --PortRV {PortRV} \
+            --HostR {HostR} \
+            --PortR {PortR}')
--- build/lib/ailever/utils/_dashboard.py
+++ build/lib/ailever/utils/_dashboard.py
@@ -1,9 +1,40 @@
 import os
+import argparse
 from urllib.request import urlretrieve
+parser = argparse.ArgumentParser(description="dashboard parser")
+parser.add_argument('--HostDash', type=str, required=False, default='PassToken', help="host : dashboard")
+parser.add_argument('--PortDash', type=str, required=False, default='PassToken', help="port : dashboard")
+parser.add_argument('--HostDB', type=str, required=False, default='PassToken', help="host : database")
+parser.add_argument('--PortDB', type=str, required=False, default='PassToken', help="port : database")
+parser.add_argument('--HostRV', type=str, required=False, default='PassToken', help="host : real-time visualization")
+parser.add_argument('--PortRV', type=str, required=False, default='PassToken', help="port : real-time visualization")
+parser.add_argument('--HostR', type=str, required=False, default='PassToken', help="host : language r")
+parser.add_argument('--PortR', type=str, required=False, default='PassToken', help="port : language r")
+args = parser.parse_args()
 
-def dashboard(name, host='127.0.0.1', port='8050'):
+def dashboard(name='main',
+              HostDash=args.HostDash,
+              PortDash=args.PortDash,
+              HostDB=args.HostDB,
+              PortDB=args.PortDB,
+              HOSTRV=args.HostRV,
+              PortRV=args.PortRV,
+              HostR=args.HostR,
+              PortR=args.PortR,
+              ):
     if not os.path.isfile(f'{name}.py'):
         urlretrieve('https://raw.githubusercontent.com/ailever/openapi/master/utils/'+name+'.py', f'./{name}.py')
         print(f'[AILEVER] The file "{name}.py" is downloaded!')
-    os.system(f'python {name}.py --ds {host} --dp {port}')
+    else:
+        raise Exception("[AILEVER] Download is failed.")
+
+    os.system(f'python {name}.py \
+            --HostDash {HostDash} \
+            --PortDash {PortDash} \
+            --HostDB {HostDB} \
+            --PortDB {PortDB} \
+            --HostRV {HostRV} \
+            --PortRV {PortRV} \
+            --HostR {HostR} \
+            --PortR {PortR}')
 
