26d29725b528ffdf74446c7ade3ad6f62b1f6cc7
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-07-21 19:50:15 +0900

    update

--- ailever/analysis/ANALYSISInterfaces.py
+++ ailever/analysis/ANALYSISInterfaces.py
@@ -1,15 +1,10 @@
-import os
 from .exploratory_data_analysis import ExploratoryDataAnalysis
 from .exploratory_data_analysis import Counting
 
-def EDA(frame, path='ExploratoryDataAnalysis', save=False):
-    if not os.path.isdir(path):
-        os.mkdir(path)
-    
-    return ExploratoryDataAnalysis(frame)
+def EDA(frame, save=False, path='ExploratoryDataAnalysis'):
+    return ExploratoryDataAnalysis(frame=frame)
 
-
-def counting(frame, path=None, save=False):
-    frame = Counting(frame)
-    frame = frame.CountsByInstance(path=path, save=save)
+def counting(frame, view='table', save=False, path=None):
+    counter = Counting(frame=frame)
+    frame = counter.CountsUniqueValues(view=view, save=save, path=path)
     return frame
--- ailever/analysis/exploratory_data_analysis.py
+++ ailever/analysis/exploratory_data_analysis.py
@@ -27,21 +27,36 @@ class Counting:
         self.frame = frame
         self.path = path
 
-    def CountsByInstance(self, path=None, save=False):
-        Base = pd.DataFrame(columns=['Column', 'Instance', 'Count'])
-        for column in self.frame.columns:
-            base = self.frame[column].value_counts().reset_index().rename(columns={'index':'Instance', column:'Count'})
-            base.insert(0, 'Column', column)
-            Base = Base.append(base)
+    def CountUniqueValues(self, view='table', save=False, path=None):
+        if view == 'table':
+            data = list()
+            for column in self.frame.columns:
+                data.append([column, red_wine.value_counts(column).shape[0]])
+            EDAframe = pd.DataFrame(data, columns=['Column', 'NumUniqueInstance'])
+            _saveing(EDAframe, save, self.path, path, 'CountColumns.csv')
 
-        if save:
-            if not path:
-                if not os.path.isdir(self.path):
-                    os.mkdir(self.path)
-                Base.to_csv(os.path.join(self.path, 'CountsByInstance.csv'))
-            else:
-                if not os.path.isdir(path):
-                    os.mkdir(path)
-                Base.to_csv(os.path.join(path, 'CountsByInstance.csv'))
+        elif view == 'column':
+            EDAframe = pd.DataFrame(columns=['Column', 'Instance', 'Count'])
+            for column in self.frame.columns:
+                base = self.frame[column].value_counts().reset_index().rename(columns={'index':'Instance', column:'Count'})
+                base.insert(0, 'Column', column)
+                EDAframe = EDAframe.append(base)
+            _saveing(EDAframe, save, self.path, path, 'CountInstances.csv')
 
-        return Base
+        return EDAframe
+
+
+def _saving(frame, save, default_path, priority_path, name):
+    if save:
+        if not priority_path:
+            if not os.path.isdir(default_path):
+                os.mkdir(default_path)
+            frame.to_csv(os.path.join(default_path, name))
+            print(f'[AILEVER] The file {name} is saved at {default_path}.')
+
+        else:
+            if not os.path.isdir(priority_path):
+                os.mkdir(priority_path)
+            frame.to_csv(os.path.join(priority_path, name))
+            print(f'[AILEVER] The file {name} is saved at {priority_path}.')
+
