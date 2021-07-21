bb18482ea7c401fcf85ee9f7d4c6c4be00ef12a8
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-07-21 23:36:36 +0900

    update

--- build/lib/ailever/analysis/exploratory_data_analysis.py
+++ build/lib/ailever/analysis/exploratory_data_analysis.py
@@ -49,8 +49,8 @@ class ExploratoryDataAnalysis:
         else:
             table = self.frame
 
-        table_definition = pd.DataFrame(columns=['NumRows', 'NumColumns', 'NumNumericColumnType', 'NumCategoricalColumnType', ])
-
+        base_columns = ['NumRows', 'NumColumns', 'NumNumericColumnType', 'NumCategoricalColumnType', ]
+        table_definition = pd.DataFrame(columns=base_columns)
         C = 0; N = 0
         for column in table.columns:
             ColumnType = 'Letter' if table[column].dtype == 'object' else 'Number'
@@ -58,8 +58,8 @@ class ExploratoryDataAnalysis:
                 C += 1
             else:
                 N += 1
-
-        table_definition = table_definition.append(pd.DataFrame(data=[[table.shape[0], table.shape[1], N, C]], columns=['NumRows', 'NumColumns', 'NumNumericColumnType', 'NumCategoricalColumnType', ]))
+        a_row = pd.DataFrame(data=[[table.shape[0], table.shape[1], N, C]], columns=base_columns)
+        table_definition = table_definition.append(a_row)
         _csv_saving(table_definition, save, self.path, path, 'EDA_TableDefinition.csv')
         return table_definition
 
@@ -71,15 +71,14 @@ class ExploratoryDataAnalysis:
 
         base_columns = ['Column', 'ColumnType', 'NumUniqueInstance', 'NumMV', 'DataType', 'DataExample', 'MaxInstanceLength']
         attributes_matrix = pd.DataFrame(columns=base_columns)
-
         for column in table.columns:
             ColumnType = 'Letter' if table[column].dtype == 'object' else 'Number' 
             NumUniqueInstance = table[column].value_counts().shape[0]
             MaxInstanceLength = table[column].astype('str').apply(lambda x: len(x)).max()
             NumMV = table[column].isna().sum()
             DataType = table[column].dtype.type
-            row = pd.DataFrame(data=[[column, ColumnType, NumUniqueInstance, NumMV, DataType, table[column].iloc[np.random.randint(NumUniqueInstance)], MaxInstanceLength]], columns=base_columns)
-            attributes_matrix = attributes_matrix.append(row)
+            a_row = pd.DataFrame(data=[[column, ColumnType, NumUniqueInstance, NumMV, DataType, table[column].iloc[np.random.randint(NumUniqueInstance)], MaxInstanceLength]], columns=base_columns)
+            attributes_matrix = attributes_matrix.append(a_row)
 
         attributes_matrix.insert(2, 'NumRows', table.shape[0])
         attributes_matrix.insert(4, 'IdealSymmericCount', table.shape[0]/attributes_matrix['NumUniqueInstance'])
@@ -89,6 +88,30 @@ class ExploratoryDataAnalysis:
         _csv_saving(attributes_matrix, save, self.path, path, 'EDA_AttributesSpecification.csv')
         return attributes_matrix
 
+    def univariate_frequency(self, priority_frame=None, save=False, path=None):
+        if priority_frame is not None:
+            table = priority_frame
+        else:
+            table = self.frame
+
+        base_columns = ['Column', 'Instance', 'NumUniqueInstance', 'Count', 'Rank']
+        frequency_matrix = pd.DataFrame(columns=base_columns)
+        for column in table.columns:
+            instance_count = table.value_counts(column, ascending=False).to_frame()
+            rank_mapper = instance_count.rank(ascending=False)
+            instance_frequency = instance_count.sort_index().reset_index().rename(columns={column:'Instance', 0:'Count'})
+            instance_frequency.insert(0, 'Column', column)
+            instance_frequency.insert(2, 'NumUniqueInstance', instance_count.shape[0])
+            instance_frequency.insert(4, 'Rank', instance_frequency.Instance.apply(lambda x: rank_mapper.loc[x]))
+            frequency_matrix = frequency_matrix.append(instance_frequency)
+
+        NumRows = table.shape[0]
+        frequency_matrix.insert(2, 'NumRows', NumRows)
+        frequency_matrix.insert(4, 'IdealSymmericCount', NumRows/frequency_matrix.NumUniqueInstance)
+        frequency_matrix.insert(6, 'IdealSymmericRatio', 1/frequency_matrix.NumUniqueInstance)        
+        frequency_matrix.insert(7, 'Ratio', frequency_matrix.Count/NumRows)
+        _csv_saving(attributes_matrix, save, self.path, path, 'EDA_UnivariateFrequencyAnalysis.csv')
+        return frequency_matrix
 
 
 
