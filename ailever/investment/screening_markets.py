e2dab9fabb5e3cccb8d89cfebb549a8211accfb8
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-07-31 13:37:19 +0900

    update

--- ailever/investment/screening_markets.py
+++ ailever/investment/screening_markets.py
@@ -10,29 +10,7 @@ def reits_screening(period, path):
     for idx, csv in enumerate(os.listdir(path)):
         idx2csv[idx] = csv
         if idx == 0 :from github import Github
-g = Github("username", "password")
 
-repo = g.get_user().get_repo(GITHUB_REPO)
-all_files = []
-contents = repo.get_contents("")
-while contents:
-    file_content = contents.pop(0)
-    if file_content.type == "dir":
-        contents.extend(repo.get_contents(file_content.path))
-    else:
-        file = file_content
-        all_files.append(str(file).replace('ContentFile(path="','').replace('")',''))
-
-with open('/tmp/file.txt', 'r') as file:
-    content = file.read()
-
-# Upload to github
-git_prefix = 'folder1/'
-git_file = git_prefix + 'file.txt'
-if git_file in all_files:
-    contents = repo.get_contents(git_file)
-    repo.update_file(contents.path, "committing files", content, contents.sha, branch="master")
-    print(git_file + ' UPDATED')
 else:
     repo.create_file(git_file, "committing files", content, branch="master")
     print(git_file + ' CREATED')
