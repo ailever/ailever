724e2ad81ae4c127eff6537d40cccd1ccd0a1a4c
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-07-31 16:06:10 +0900

    update

--- ailever/investment/screening_markets.py
+++ ailever/investment/screening_markets.py
@@ -1,19 +1,24 @@
+from .parallelizer import parallelize
+
 import os
 import numpy as np
 from numpy import linalg
 import pandas as pd
 
-def reits_screening(period, path):
-    print(f'[AILEVER] Recommandations based on latest {period}')
-    idx2csv = dict()
 
-    for idx, csv in enumerate(os.listdir(path)):
-        idx2csv[idx] = csv
-        if idx == 0 :
-            base = pd.read_csv(path+csv)['close'][-period:].fillna(method='bfill').fillna(method='ffill').values[:,np.newaxis]
-        else:
-            appending = pd.read_csv(path+csv)['close'][-period:].fillna(method='bfill').fillna(method='ffill').values[:,np.newaxis]
-            base = np.c_[base, appending]
+
+def reits_screening(path, period):
+    print(f'[AILEVER] Recommandations based on latest {period} days.')
+
+    prllz = parallelize(path=path,
+                        object_format='csv',
+                        base_column='close',
+                        date_column='date',
+                        truncate=period)
+
+    base = prllz.ndarray
+    tickers = prllz.pdframe.columns
+    mapper = {idx:ticker for idx, ticker in enumerate(tickers)}
 
     x, y = np.arange(base.shape[0]), base
     bias = np.ones_like(x)
@@ -22,5 +27,5 @@ def reits_screening(period, path):
     b = linalg.inv(X.T@X) @ X.T @ y
     yhat = X@b
     recommand = yhat[-1] - yhat[-2]
-    return list(map(lambda x: idx2csv[x][:-4], np.argsort(recommand)[::-1]))
+    return list(map(lambda x: mapper[x][:-4], np.argsort(recommand)[::-1]))
 
--- build/lib/ailever/investment/parallelizer.py
+++ build/lib/ailever/investment/parallelizer.py
@@ -6,14 +6,20 @@ import re
 import numpy as np
 import pandas as pd
 
-def parallelize(path='.', object_format='csv'):
-    prllz = Parallelizer(path=refine(path), object_format=object_format)
+def parallelize(path='.', object_format='csv', base_column='close', date_column='date', truncate=100):
+    prllz = Parallelizer(path=path,
+                         object_format=object_format,
+                         base_column=base_column,
+                         date_column=date_column,
+                         truncate=truncate)
     return prllz
 
 class Parallelizer:
-    def __init__(self, path, object_format, truncate=100):
+    def __init__(self, path, object_format, base_column, date_column, truncate):
         self.origin_path = os.getcwd()
-        self.serialization_path = path
+        self.serialization_path = refine(path)
+        self.base_column = base_column
+        self.date_column = date_column
         self.truncated_period = truncate
         self.ndarray = getattr(self, '_'+object_format)(to='ndarray')
         self.pdframe = getattr(self, '_'+object_format)(to='pdframe')
@@ -24,32 +30,32 @@ class Parallelizer:
         
         so_path = os.path.join(self.serialization_path, serialized_objects.pop(0))
         base_init_frame = pd.read_csv(so_path)
-        base_period = base_init_frame['date'].values[-self.truncated_period:]
-        base_array = base_init_frame['close'].values[-self.truncated_period:]
+        base_period = base_init_frame[self.date_column].values[-self.truncated_period:]
+        base_array = base_init_frame[self.base_column].values[-self.truncated_period:]
 
         mismatching = list()
         for so in serialized_objects:
             so_path = os.path.join(self.serialization_path, so)
             appending_frame = pd.read_csv(so_path)
-            appending_period = appending_frame['date'].values[-self.truncated_period:]
+            appending_period = appending_frame[self.date_column].values[-self.truncated_period:]
             checker = base_period == appending_period
             if not checker.all():
                 so = so[:-re.search('[.]', so[::-1]).span()[1]]
                 mismatching.append(so)
                 continue
-            appending_array = appending_frame['close'].values[-self.truncated_period:]
+            appending_array = appending_frame[self.base_column].values[-self.truncated_period:]
             base_array = np.c_[base_array, appending_array]
         
         for m_ticker in mismatching:
             del ticker_names[ticker_names.index(m_ticker)]
 
         if to == 'ndarray':
-            print('* Mismatched Tickers for given period :', mismatching)
+            print('[AILEVER] Mismatched tickers for given period :', mismatching)
             return base_array
 
         elif to == 'pdframe':
             base_frame = pd.DataFrame(data=base_array, columns=ticker_names)
-            base_frame.insert(0, 'date', pd.to_datetime(base_period))
-            return base_frame.set_index('date')
+            base_frame.insert(0, self.date_column, pd.to_datetime(base_period))
+            return base_frame.set_index(self.date_column)
 
     
