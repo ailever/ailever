81b776bdc73e8d9db5d6d9685e2ec5e3a3cf493a
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-03 00:47:50 +0900

    update

--- build/lib/ailever/investment/finance_datasets.py
+++ build/lib/ailever/investment/finance_datasets.py
@@ -2,8 +2,10 @@ from ..path import refine
 
 import os
 import json
+from tqdm import tqdm
 import pandas as pd
 import FinanceDataReader as fdr
+from yahooquery import Ticker
 
 def integrated_loader(baskets, path=False):
     if path:
@@ -22,7 +24,18 @@ def integrated_loader(baskets, path=False):
     existed_securities = filter(lambda x: x in download_log, baskets)
     not_existed_securities = filter(lambda x: not x in download_log, baskets)
 
-    loader.from_fdr(not_existed_securities)
+    # priority 1 : yahooquery
+    print('* from yahooquery')
+    loader.from_yahooquery(baskets=not_existed_securities, country='united states', progress=True)
+    if bool(loader.failures):
+        # priority 2 : finance datareader
+        print('* from finance-datareader')
+        loader.from_fdr(baskets=not_existed_securities)
+        if bool(loader.failures):
+            # priority 3 : ?
+            pass
+
+    # Final Load : From Local
     if not bool(loader.failures):
         return loader.from_local(baskets)
     else:
@@ -64,10 +77,39 @@ class Loader:
             dataset[security] = pd.read_csv(os.path.join(self.dataset_dirname, f'{security}.csv'))
         return dataset
 
+    def from_yahooquery(self, baskets, asynchronouse=False, backoff_factor=0.3, country='united states',
+                        formatted=False, max_workers=8, proxies=None, retry=5, status_forcelist=[404, 429, 500, 502, 503, 504], timeout=5,
+                        validate=False, verify=True, progress=True):
+        successes = list()
+        failures = list()
+        ticker = Ticker(symbols=baskets, asynchronouse=asynchronouse, backoff_factor=backoff_factor, country=country,
+                        formatted=formatted, max_workers=max_workers, proxies=proxies, retry=retry, status_forcelist=status_forcelist, timeout=timeout,
+                        validate=validate, verify=verify, progress=progress)
+        securities = ticker.history(period="ytd", interval="1d", start=None, end=None, adj_timezone=True, adj_ohlc=True)
+
+        if isinstance(securities, pd.core.frame.DataFrame):
+            be_in_memory = set(map(lambda x:x[0], securities.index))
+            successes.extend(be_in_memory)
+            failures.extend(filter(lambda x: not x in be_in_memory, baskets))
+            for security in be_in_memory:
+                securities.loc[security].to_csv(os.path.join(self.dataset_dirname, f'{security}.csv'))
+        elif isinstance(securities, dict):
+            be_in_memory = map(lambda x:x[0], filter(lambda x:not isinstance(x[1], str), zip(securities.keys(), securities.values())))
+            not_in_memory = map(lambda x:x[0], filter(lambda x:isinstance(x[1], str), zip(securities.keys(), securities.values())))
+            successes.extend(be_in_memory)
+            failures.extend(not_in_memory)
+            for security in successes:
+                fdr.DataReader(security).to_csv(os.path.join(self.dataset_dirname, f'{security}.csv'))
+
+        self.successes.update(successes)
+        self.failures.update(failures)
+        self._logger_for_successes('from_yahooquery')
+
+
     def from_fdr(self, baskets):
         successes = list()
         failures = list()
-        for security in baskets:
+        for security in tqdm(baskets):
             try:
                 fdr.DataReader(security).to_csv(os.path.join(self.dataset_dirname, f'{security}.csv'))
                 successes.append(security)
