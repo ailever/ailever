8674573a6cb909c0355fa5fcbb0b53749939ec90
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-04 01:50:47 +0900

    update

--- build/lib/ailever/investment/INVESTMENTInterfaces.py
+++ build/lib/ailever/investment/INVESTMENTInterfaces.py
@@ -1,5 +1,5 @@
 from .initializer import initialize
-from .finance_datasets import integrated_loader
+from .finance_datasets import integrated_dataloader
 from .parallelizer import parallelize
 from .screening_markets import reits_screening
 from .portfolio_optimization import portfolio_optimize
--- build/lib/ailever/investment/__init__.py
+++ build/lib/ailever/investment/__init__.py
@@ -1,6 +1,6 @@
 from .INVESTMENTInterfaces import initialize
 from .INVESTMENTInterfaces import parallelize
-from .INVESTMENTInterfaces import integrated_loader
+from .INVESTMENTInterfaces import integrated_dataloader
 from .INVESTMENTInterfaces import reits_screening
 from .INVESTMENTInterfaces import portfolio_optimize
 
