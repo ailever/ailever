b53dd9ee50d5ed863c7746296c4c73266572ccd2
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-04 16:34:32 +0900

    forecasting_models

--- ailever/investment/logger_check.py
+++ ailever/investment/logger_check.py
@@ -5,11 +5,10 @@ from ailever.investment import integrated_loader
 
 
 def status_finance_datasets(log_file=".dataset_log.json", log_path=r"./", baskets, path, on_assets):
-
     if not os.isfile(os.path.join(log_path, log_file):
         integrated_loader(baskets, path, on_assets)
     with open(os.path.join(log_path, log_file),'r') as log:
-            download_log = json.loads(json.load(log))
+        download_log = json.loads(json.load(log))
     
     download_log_times = list(download_log.keys())
     format_time = '%Y-%m-%d %H:%M:%S'
