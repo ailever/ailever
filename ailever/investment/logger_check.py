b91cd94213c1c05e63508e98ab9488e475de6345
hvagabond <dbs0415@gmail.com>
2021-08-04 22:21:17 +0900

    logger and finacedatasets

--- ailever/investment/logger_check.py
+++ ailever/investment/logger_check.py
@@ -1,21 +1,36 @@
 import json
 import os
 from datetime import datetime
-from ailever.investment import integrated_loader
+from .finance_datasets import integrated_dataloader
+from pytz import timezone
 
+"""
+EST 09:30 ~ 16:00
+"""
 
-def status_finance_datasets(log_file=".dataset_log.json", log_path=r"./", baskets, path, on_assets):
-    if not os.isfile(os.path.join(log_path, log_file):
-        integrated_loader(baskets, path, on_assets)
-    with open(os.path.join(log_path, log_file),'r') as log:
-        download_log = json.loads(json.load(log))
+def financedatasets_logger(baskets=False, path=False, source="yahooquery", log_file=".dataset_log.json", log_path=r"./"):
     
-    download_log_times = list(download_log.keys())
-    format_time = '%Y-%m-%d %H:%M:%S'
-    download_log_times = list(map(lambda x: datetime.strptime(x, format_time)
+    assert baskets, "No baskets input will lead to no change"
 
-    if datetime.today().date() >= max(download_log_times).date():
-        integrated_loader(baskets, path, on_assets)
+    if not os.path.isfile(os.path.join(log_path, log_file)):
+        return integrated_dataloader(baskets, path, source)
+        
 
+    with open(os.path.join(log_path, log_file),'r') as log:
+            download_log = json.loads(json.load(log))
+    
+    if not baskets in download_log.keys():
+        return integrated_dataloader(baskets, path, source)
+        
 
+    in_basket_values = list(map(download_log.get, baskets))
+    in_basket_dates = [value["Table_EndDate"] for value in in_basket_values]
+
+    if None in in_basket_dates:
+        return integrated_dataloader(baskets, path, source)
+        
+    format_time = '%Y-%m-%d'
+    if datetime.now(timezone('US/Eastern')) > max(list(map(lambda x: datetime.strptime(x, format_time), in_basket_dates))):
+        return integrated_dataloader(baskets, path, source)
+        
 
