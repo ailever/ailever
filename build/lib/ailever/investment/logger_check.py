addfe25b1df64579bc511de08d3625c2e65ba63e
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-06 14:01:12 +0900

    update

--- build/lib/ailever/investment/logger_check.py
+++ build/lib/ailever/investment/logger_check.py
@@ -21,26 +21,41 @@ dataset_dirname = os.path.join(base_dir['root'], base_dir['rawdata_repository'])
 log_dirname = os.path.join(base_dir['root'], base_dir['metadata_store'])
 
 def ohlcv_update(baskets=None, path=dataset_dirname, source="yahooquery", log_file=".dataset_log.json", log_path=log_dirname):
+    
+    if not baskets:
+        # Case 1 ) No log file - Initiate
+        if not os.path.isfile(os.path.join(log_path, log_file)):
+            
+            # Case 1-1) No downdownladed ever -> assert
+            assert os.path.isdir(path) or len(os.listdir(path))==0, "First time data downloading - Please select baskets"
+
+            # Case 1-2) data downloader from outsources -> all tickers update and write logging
+            serialized_objects = os.listdir(path)
+            serialized_objects = list(filter(lambda x: x[-3:] == 'csv', serialized_objects)
+            tickers = list(map(lambda x: x[-re.search('[.]', x[::-1]).span()[1]], serialized_objects))
+            return ohlcv_dataloader(tickers, path, source)
         
-    # Case 1) No log file - Initiate
+            
+    
+    # Case 2) No log file - Initiate
     if not os.path.isfile(os.path.join(log_path, log_file)):
         return ohlcv_dataloader(baskets, path, source)
     
     with open(os.path.join(log_path, log_file),'r') as log:
             download_log = json.loads(json.load(log))
     
-    # Case 2) When any tickers in baskets are not in existing logger -> tickers in baskets all renew
+    # Case 3) When any tickers in baskets are not in existing logger -> tickers in baskets all renew
     if not baskets in list(download_log.keys()):
         return ohlcv_dataloader(baskets, path, source)
         
     in_basket_values = list(map(download_log.get, baskets))
     in_basket_dates = [value["Table_EndDate"] for value in in_basket_values]
 
-    # Case 3) When no Table end date is put it (eg. when log file was newly made with in-place outsourced csv files)
+    # Case 4) When no Table end date is put it (eg. when log file was newly made with in-place outsourced csv files)
     if None in in_basket_dates:
         return ohlcv_dataloader(baskets, path, source)
     
-    # Case 4) all tickers in basket was in exisitng logger but they are outdated
+    # Case 5) all tickers in basket was in exisitng logger but they are outdated
     format_time = '%Y-%m-%d'
     if datetime.now(timezone('US/Eastern')) > max(list(map(lambda x: datetime.strptime(x, format_time), in_basket_dates))):
         return ohlcv_dataloader(baskets, path, source)
