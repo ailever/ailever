068dbab70ee7f97472ad6cafc816c97649d87f97
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-06 16:12:14 +0900

    update

--- ailever/investment/logger_check.py
+++ ailever/investment/logger_check.py
@@ -34,32 +34,32 @@ def ohlcv_update(baskets=None, path=dataset_dirname, log_file=".dataset_log.json
             serialized_objects = os.listdir(path)
             serialized_objects = list(filter(lambda x: x[-3:] == 'csv', serialized_objects))
             tickers = list(map(lambda x: x[:-4], serialized_objects))
-            return ohlcv_dataloader(baskets=tickers, path=path, log_path=log_path, source=source)
+            return ohlcv_dataloader(baskets=tickers, source=source)
         
             
     
     # Case 2) No log file - Initiate
     if not os.path.isfile(os.path.join(log_path, log_file)):
-        return ohlcv_dataloader(baskets=baskets, path=path, log_path=log_path, source=source)
+        return ohlcv_dataloader(baskets=baskets, source=source)
     
     with open(os.path.join(log_path, log_file),'r') as log:
             download_log = json.loads(json.load(log))
 
     # Case 3) When any tickers in baskets are not in existing logger -> tickers in baskets all renew
     if not baskets in list(download_log.keys()):
-        return ohlcv_dataloader(baskets=baskets, path=path, log_path=log_path, source=source)
+        return ohlcv_dataloader(baskets=baskets, source=source)
         
     in_basket_values = list(map(download_log.get, baskets))
     in_basket_dates = [value["Table_EndDate"] for value in in_basket_values]
 
     # Case 4) When no Table end date is put it (eg. when log file was newly made with in-place outsourced csv files)
     if None in in_basket_dates:
-        return ohlcv_dataloader(baskets=baskets, path=path, log_path=log_path, source=source)
+        return ohlcv_dataloader(baskets=baskets, source=source)
     
     # Case 5) all tickers in basket was in exisitng logger but they are outdated
     format_time = '%Y-%m-%d'
     if datetime.now(timezone('US/Eastern')) > max(list(map(lambda x: datetime.strptime(x, format_time), in_basket_dates))):
-        return ohlcv_dataloader(baskets=basekts, path=path, log_path=log_path, source=source)
+        return ohlcv_dataloader(baskets=basekts, source=source)
         
 
 
