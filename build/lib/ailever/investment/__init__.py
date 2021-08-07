863e775346ef28e3d6b268e55e7d5d2f6674e117
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-08 07:18:47 +0900

    update

--- build/lib/ailever/investment/__init__.py
+++ build/lib/ailever/investment/__init__.py
@@ -1,11 +1,11 @@
 from .INVESTMENTInterfaces import initialize
 from .INVESTMENTInterfaces import parallelize
-from .INVESTMENTInterfaces import screener
 from .INVESTMENTInterfaces import portfolio_optimize
 from .INVESTMENTInterfaces import Forecaster
 from .INVESTMENTInterfaces import sectors
 from .INVESTMENTInterfaces import Loader
 from .INVESTMENTInterfaces import Preprocessor
+from .INVESTMENTInterfaces import screener
 
 from ._fmlops_policy import local_initialization_policy
 
