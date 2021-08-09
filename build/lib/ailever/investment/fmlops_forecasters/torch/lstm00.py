cfd921683a9e2fb9a2b0d7ec4f18cb0561d240b0
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-09 22:48:56 +0900

    update

--- build/lib/ailever/investment/fmlops_forecasters/torch/lstm00.py
+++ build/lib/ailever/investment/fmlops_forecasters/torch/lstm00.py
@@ -65,8 +65,8 @@ class InvestmentDataset(Dataset):
         self.train_range = self.packet_size - self.predict_range
         
         self.frame = ohlcv_dataloader(baskets=[ticker]).dict[ticker][__fmlops_bs__.rawdata_repository.base_columns]
-        self.frame = self.frame.set_index('date')
         self.frame.date = pd.to_datetime(self.frame.date.astype('str'))
+        self.frame = self.frame.set_index('date')
 
         self.frame_train = self.frame.iloc[:1500]
         self.frame_test = self.frame.iloc[1500:]
