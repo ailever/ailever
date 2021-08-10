0316ff842c162fe2d7dce822dfd7dcf258715a5f
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-10 22:09:26 +0900

    update policy

--- ailever/investment/_fmlops_policy.py
+++ ailever/investment/_fmlops_policy.py
@@ -139,6 +139,7 @@ def local_initialization_policy(local_environment:dict=None):
     fmlops_bs.local_system.root.model_registry = fmlops.hierarchy('model_registry')
     fmlops_bs.local_system.root.metadata_store = fmlops.hierarchy('metadata_store')
     fmlops_bs.local_system.root.metadata_store.model_specifications = fmlops.hierarchy('model_specifications')
+
     fmlops_bs.local_system.root.rawdata_repository.base_columns = ['date', 'close', 'volume']
 
     
