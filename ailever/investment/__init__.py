a8ad4fe82f4c4a3579aadcef18e535b26d90f97d
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-10 19:23:49 +0900

    update

--- ailever/investment/__init__.py
+++ ailever/investment/__init__.py
@@ -1,6 +1,14 @@
 from ._fmlops_policy import local_initialization_policy
 from ._fmlops_policy import fmlops_bs as __fmlops_bs__
-local_initialization_policy()
+
+local_environment = dict()
+local_environment['root'] = __fmlops_bs__.local_system.root.name
+local_environment['rawdata_repository'] = __fmlops_bs__.local_system.root.rawdata_repository.name
+local_environment['feature_store'] = __fmlops_bs__.local_system.root.feature_source.name
+local_environment['source_repository'] = __fmlops_bs__.local_system.root.source_repository.name
+local_environment['model_registry'] = __fmlops_bs__.local_system.root.model_registry.name
+local_environment['metadata_store'] = __fmlops_bs__.local_system.root.metadata_store.name
+local_initialization_policy(local_environment)
 
 from .INVESTMENTInterfaces import initialize
 from .INVESTMENTInterfaces import parallelize
