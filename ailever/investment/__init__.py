3cbff2f629cfa1c9b1f57737d3fe68c56a6a9ff6
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-08-10 19:24:45 +0900

    update

--- ailever/investment/__init__.py
+++ ailever/investment/__init__.py
@@ -4,7 +4,7 @@ from ._fmlops_policy import fmlops_bs as __fmlops_bs__
 local_environment = dict()
 local_environment['root'] = __fmlops_bs__.local_system.root.name
 local_environment['rawdata_repository'] = __fmlops_bs__.local_system.root.rawdata_repository.name
-local_environment['feature_store'] = __fmlops_bs__.local_system.root.feature_source.name
+local_environment['feature_store'] = __fmlops_bs__.local_system.root.feature_store.name
 local_environment['source_repository'] = __fmlops_bs__.local_system.root.source_repository.name
 local_environment['model_registry'] = __fmlops_bs__.local_system.root.model_registry.name
 local_environment['metadata_store'] = __fmlops_bs__.local_system.root.metadata_store.name
