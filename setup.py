f289fbee7899c94b2595e2c1a08972c7e6f25522
ailever <ailever.group@gmail.com>
2021-08-14 15:27:32 +0900

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name                = 'ailever',
-      version             = '0.2.675',
+      version             = '0.2.676',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
