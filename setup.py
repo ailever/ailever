2e9f8db0c6eaa21905f9bdc5c2ebefbbd0a65137
hvagabond <dbs0415@gmail.com>
2021-08-14 23:05:57 +0900

    update

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name                = 'ailever',
-<<<<<<< HEAD
-      version             = '0.2.696',
-=======
-      version             = '0.2.695',
->>>>>>> fddcc4c8d2a73613507e3370497e0d3658d50fe9
+      version             = '0.2.697',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
