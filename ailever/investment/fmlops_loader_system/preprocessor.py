0fb3600c5d1cdec8610002896953f1cf6f82226a
hvagabond <dbs0415@gmail.com>
2021-08-14 23:10:37 +0900

    dict key fix for date featuring

--- ailever/investment/fmlops_loader_system/preprocessor.py
+++ ailever/investment/fmlops_loader_system/preprocessor.py
@@ -80,7 +80,7 @@ class Preprocessor(DataTransferCore):
 
     def date_featuring(self):
         
-        date_index = pd.to_datetime(self.dict[self.dict.keys()[0]].index.to_series())
+        date_index = pd.to_datetime(self.dict[list(self.dict.keys())[0]].index.to_series())
         date_featured = pd.concat([date_index.apply(lambda x: x.year), date_index.apply(lambda x: x.month), date_index.apply(lambda x: x.day), date_index.apply(lambda x: x.dt.dayofweek)], axis=1)
         for ticker in list(self.dict.keys()):
             merged_frame = date_featured.merge(self.dict[ticker], how='outer', left_index=True, right_index=True)
