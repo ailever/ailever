ea3c897148864ed0daf410327c363a74f9652e09
ailever <ailever.group@gmail.com>
2021-08-14 15:17:38 +0900

    update

--- build/lib/ailever/investment/temp_loader.py
+++ build/lib/ailever/investment/temp_loader.py
@@ -12,16 +12,6 @@ import pandas as pd
 import FinanceDataReader as fdr
 from yahooquery import Ticker
 
-base_dir = dict()
-base_dir['root'] = fmlops_bs.local_system.root.name
-base_dir['rawdata_repository'] = fmlops_bs.local_system.root.rawdata_repository.name
-base_dir['metadata_store'] = fmlops_bs.local_system.root.metadata_store.name
-base_dir['feature_store'] = fmlops_bs.local_system.root.feature_store.name
-base_dir['model_registry'] = fmlops_bs.local_system.root.model_registry.name
-base_dir['source_repotitory'] = fmlops_bs.local_system.root.source_repository.name
-
-dataset_dirname = os.path.join(base_dir['root'], base_dir['rawdata_repository'])
-log_dirname = os.path.join(base_dir['root'], base_dir['metadata_store'])
 
 def ohlcv_dataloader(baskets:Iterable[str], path:str=dataset_dirname, log_path:str=log_dirname, source:str='yahooquery')->DataTransferCore:
     if not path:
