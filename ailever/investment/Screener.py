d2f5b323ee6c2335ac7ef471b9ba6a9d641c458e
hvagabond <dbs0415@gmail.com>
2021-08-17 01:13:32 +0900

    update

--- ailever/investment/Screener.py
+++ ailever/investment/Screener.py
@@ -4,6 +4,7 @@ from .parallelizer import parallelize
 from ._base_transfer import DataTransferCore
 from .logger import Logger
 from .fmlops_loader_system import Loader
+from .fmlops_loader_system import Preprocessor
 from .fmlops_loader_system.DataVendor import DataVendor
 from pytz import timezone
 
@@ -38,10 +39,7 @@ class Screener(DataTransferCore):
         sory_by option
         ['DividendYield', 'FiveYrsDividendYield', 'DividendRate', 'Beta', 'EVtoEBITDA', 'Marketcap', 'EnterpriseValue']"""
         module_dict = Screener.fundamentals_modules_fromyahooquery_dict
-        order_type = {'DividendYield': True, 'FiveYrsDiviendYield': False, 'DiviendRate': False, 'Beta': True, 'EVtoEBITDA': False, 'Marketcap': False, 'EnterpriseValue': False}
-        if not period:
-            period = 10
-            logger.normal_logger.info(f'[SCREENER] PERIOD INPUT REQUIRED - Default Period:{period}')       
+        order_type = {'DividendYield': True, 'FiveYrsDiviendYield': False, 'DiviendRate': False, 'Beta': True, 'EVtoEBITDA': True, 'Marketcap': False, 'EnterpriseValue': False}
         if not from_dir:
             path = from_dir   
             logger.normal_logger.info(f'[SCREENER] FROM_DIR INPUT REQUIRED - Default Path:{path}')
@@ -76,7 +74,7 @@ class Screener(DataTransferCore):
         results_list =  preresults_pdframe.index.tolist() 
         top10 = results_list[:10]
         results_pdframe = preresults_pdframe
-        logger.normal_logger.info('[SCREENER] {sort_by} RANK YIELED: TOP 10 {top10}')
+        logger.normal_logger.info(f'[SCREENER] {sort_by} RANK YIELED: TOP 10 {top10}')
         if output=='list':
             return results_list
         if output=='pdframe':
@@ -142,6 +140,116 @@ class Screener(DataTransferCore):
         if output=='pdframe':
             return results_pdframe
     
+    @staticmethod
+    def pct_change_screener(baskets=None, from_dir=None, to_dir=None, period=None, window=None,modules=None, sort_by=None, interval=None, country='united states', output='list'):
+        if not from_dir:
+            path = from_dir   
+            logger.normal_logger.info(f'[SCREENER] FROM_DIR INPUT REQUIRED - Default Path:{path}')
+        if not to_dir:
+            to_dir = to_dir
+            logger.normal_logger.info(f'[SCREENER] TO_DIR INPUT REQUIRED - Default Path:{from_dir}')
+        if country == 'united states':
+            today = datetime.now(timezone('US/Eastern'))
+            tz = timezone('US/Eastern')
+        if country == 'korea':
+            today = datetime.now(timezone('Asia/Seoul'))
+        if not interval:
+            interval = '1d'
+        if not window:
+            window = [1,5,20,60]
+        if not baskets: 
+            if not os.path.isfile(os.path.join(from_dir, 'fundamentals.csv')):
+                baskets_in_csv = list()
+                logger.normal_logger.info(f"[SCREENER] NO BASKETS EXISTS from {from_dir}")
+                return
+            if not os.path.isfile(os.path.join(from_dir, 'fundamentals.csv')):
+                baskets_in_csv = pd.read_csv(os.path.join(from_dir, 'fundamentals.csv'))['ticker'].tolist()         
+            baskets = baskets_in_csv ; num_baskets = len(baskets) 
+            logger.normal_logger.info(f'[SCREENER] BASKETS INPUT REQUIRED - Default Basket: {num_baskets} baskets in the directory:{from_dir}.')    
+
+        logger.normal_logger.info(f'[SCREENER] ACCESS PREPROCESSOR')
+        pre = Preprocessor()
+        preresults_dict = pre.pct_change(baskets=baskets, from_dir=from_dir, to_dir=to_dir, interval=interval, country=country, target_column='close', window=window, merge=False, kind='ticker').dict
+        main_frame_list = list()
+        for ticker in list(preresults_dict.keys()):
+            ticker_frame = preresults_dict[ticker].iloc[-1:]
+            ticker_frame['ticker'] = ticker
+            ticker_frame.reset_index(inplace=True)
+            ticker_frame.set_index('ticker', inplace=True)
+            main_frame_list.append(ticker_frame)
+        main_pdframe = pd.concat(main_frame_list, axis=0)
+        main_pdframe = main_pdframe.sort_values(sort_by)
+        results_pdframe = main_pdframe
+        results_list = main_pdframe.index.tolist()
+        top10 = results_list[:10]
+        logger.normal_logger.info(f'[SCREENER] {sort_by} RANK YIELED: TOP 10 {top10}')
+        if output=='list':
+            return results_list
+        if output=='pdframe':
+            return results_pdframe
+
+    @staticmethod
+    def momentum_screener(baskets=None, from_dir=None, interval=None, country='united stated', period=None, to_dir=None, output='list'):
+
+        if not period:
+            period = 10
+            logger.normal_logger.info(f'[SCREENER] PERIOD INPUT REQUIRED - Default Period:{period}')
+        if not from_dir:
+            from_dir = from_dir
+            logger.normal_logger.info(f'[SCREENER] FROM_DIR REQUIRED - Default Path:{from_dir}')
+        if not to_dir:
+            to_dir = to_dir
+            logger.normal_logger.info(f'[SCREENER] TO_DIR INPUT REQUIRED - Default Path:{from_dir}')
+        if not interval:
+            interval ='1d'
+            logger.normal_logger.info(f'[SCREENER] DEFAULT INTERVAL {interval}')
+        if not baskets:            
+            serialized_objects = os.listdir(from_dir)
+            serialized_object =list(filter(lambda x: (x[-3:] == 'csv') and ('_' not in x) and ("+" not in x), serialized_objects))
+            baskets_in_dir = list(map(lambda x: x[:-4], serialized_object))
+            if not baskets_in_dir:
+                logger.normal_logger.info(f'[SCREENER] NO BASKETS EXISTS in {from_dir}')
+                return
+            baskets = baskets_in_dir ; num_baskets = len(baskets) 
+            logger.normal_logger.info(f'[SCREENER] BASKETS INPUT REQUIRED - Default Basket: {num_baskets} baskets in the directory:{from_dir}.')    
+
+        logger.normal_logger.info(f'[SCREENER] UPDATE FOR BASKETS')
+        loader = Loader()
+        loader.ohlcv_loader(baskets=baskets, from_dir=from_dir, to_dir=to_dir, interval=interval, country=country)
+        
+        logger.normal_logger.info(f'[SCREENER] RECOMMANDATIONS BASED ON LATEST {period} DAYS.')
+        
+        prllz = parallelize(baskets=baskets, path=from_dir,
+                            object_format='csv',
+                            base_column='close',
+                            date_column='date',
+                            period=period)
+
+        base = prllz.ndarray
+        tickers = prllz.pdframe.columns
+        mapper = {idx:ticker for idx, ticker in enumerate(tickers)}
+
+        x, y = np.arange(base.shape[0]), base
+        bias = np.ones_like(x)
+        X = np.c_[bias, x]
+
+        b = linalg.inv(X.T@X) @ X.T @ y
+        yhat = X@b
+        recommand = yhat[-1] - yhat[-2]
+        
+        results_list = list(map(lambda x:mapper[x], np.argsort(recommand)[::-1]))
+        results_pdframe = pd.DataFrame(results_list, columns= ['ticker'])
+        recent_date = datetime.strftime(prllz.pdframe[prllz.date_column].iloc[-1], "%Y%m%d")
+        results_pdframe.to_csv(f'momentum+screener+{period}+{recent_date}.csv', index=False)
+        logger.normal_logger.info('[SCREENER] TOP 10 MOMENTUM FOR {period}: {top10}'.format(period=period, top10=results_list[:10]))
+        
+        if output=='list':
+            return results_list
+        if output=='pdframe':
+            return results_pdframe    
+
+
+
     @staticmethod
     def csv_compiler(from_dir, to_dir, now, format_time, target_list):
         return
