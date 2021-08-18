f35a5b806e21adab380866c937cc65057b24ed8e
ailever <ailever.group@gmail.com>
2021-08-18 15:58:29 +0900

    update

--- ailever/analysis/data_transformation.py
+++ ailever/analysis/data_transformation.py
@@ -35,19 +35,41 @@ class DataReduction:
 class DataDiscretizor:
     # https://pbpython.com/pandas-qcut-cut.html
     @staticmethod
-    def ew_binning(table, target_column=None, bins=4):
-        table[target_column+f'_ew_{bins}bins'] = pd.cut(table[target_column], bins=bins, precision=6).astype(str)
-        # historgam : equal width
-        # percentile : equal frequency
-        # v-optimal
-        # diff
-        return table
+    def ew_binning(table, numeric_target_columns=None, bins=4):
+        assert numeric_target_columns in table.columns, 'Target columns(numeric_target_columns) must be defined.'
+        if not isinstance(numeric_target_columns, list):
+            numeric_target_columns = list(numeric_target_columns)
+        if not isinstance(bins, list):
+            bins = list(bins)
 
-    @staticmethod
-    def ef_binning(table, target_column=None, bins=4):
-        assert target_column in table.columns, 'Target column(target_column) must be defined.'
+        for target_column in numeric_target_columns:
+            if not(table.dtypes[target_column] == float or table.dtypes[target_column] == int):
+                table[target_column] = table[target_column].astype(float)
+            for num_bin in bins:
+                _, threshold = pd.cut(table[target_column], bins=num_bin, precision=6, retbins=True)
+                table[target_column+f'_ew_{num_bin}bins'] = pd.cut(table[target_column], bins=num_bin, labels=threshold, precision=6, retbins=False).astype(float)
         
-        table[target_column+f'_ef_{bins}bins'] = pd.qcut(table[target_column], q=bins, precision=6).astype(str)
+        return table
+
+    @staticmethod
+    def ef_binning(table, numeric_target_columns=None, bins=4):
+        assert numeric_target_columns in table.columns, 'Target columns(numeric_target_columns) must be defined.'
+        if not isinstance(numeric_target_columns, list):
+            numeric_target_columns = list(numeric_target_columns)
+        if not isinstance(bins, list):
+            bins = list(bins)
+
+        for target_column in numeric_target_columns:
+            if not(table.dtypes[target_column] == float or table.dtypes[target_column] == int):
+                table[target_column] = table[target_column].astype(float)
+            for num_bin in bins:
+                _, threshold = pd.qcut(table[target_column], q=num_bin, precision=6, retbins=True)
+                table[target_column+f'_ef_{num_bin}bins'] = pd.qcut(table[target_column], q=num_bin, labels=threshold, precision=6, retbins=False).astype(float)
+
+        return table
+
+    @staticmethod
+    def opt_binning(table, target_columns=None):
         # historgam : equal width
         # percentile : equal frequency
         # v-optimal
@@ -55,28 +77,31 @@ class DataDiscretizor:
         return table
 
     @staticmethod
-    def opt_binning():
+    def diff_dw_binning(table, target_columns=None):
         # historgam : equal width
         # percentile : equal frequency
         # v-optimal
         # diff
-        return None
+        return table
 
     @staticmethod
-    def diff_binning():
+    def diff_df_binning(table, target_columns=None):
         # historgam : equal width
         # percentile : equal frequency
         # v-optimal
         # diff
-        return None
+        return table
 
     @staticmethod
-    def padding():
-        # historgam : equal width
-        # percentile : equal frequency
-        # v-optimal
-        # diff
-        return None
+    def padding(table, target_column=None, target_instance=None, non_target_pad=None):
+        assert target_column, 'Target column(target_column) must be defined.'
+        assert target_instance, 'Target instance(target_instance) must be defined.'
+
+        if non_target_pad is None:
+            table = table[target_column].apply(lambda x: x if x==target_instance else 0)
+        else:
+            table = table[target_column].apply(lambda x: x if x==target_instance else non_target_pad)
+        return table
 
     @staticmethod
     def entropy():
