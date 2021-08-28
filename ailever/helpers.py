8e1fed9373373c6b7859de8fa19066ebb8b94f63
ailever <ailever.group@gmail.com>
2021-08-28 23:53:18 +0900

    update

--- ailever/helpers.py
+++ ailever/helpers.py
@@ -2,10 +2,21 @@
 def helper(what):
     if what == "server":
         print(server)
+    elif what == "graph":
+        print(graph)
 
 server = """
 jupyter lab &
 python -m visdom.server &
 rstudio-server start
 service postgresql start
+"""
+
+graph = """
+import matplotlib as mpl
+mpl.get_cachedir()
+
+# Fonts Path
+Dwonlaod : https://hangeul.naver.com/font
+Upload : site-packages/matplotlib/mpl-data/fonts/ttf
 """
