8dec71a73fdddf35f63b220820da6f3a3cdee975
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-09-18 10:42:16 +0900

    update

--- build/lib/ailever/utils/crawler.py
+++ build/lib/ailever/utils/crawler.py
@@ -2,11 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 
 def crawl(url, tag, attr:dict=None):
-	headers = {'User-Agent': 'Mozilla/5.0'}
-	response = requests.get(url, headers=headers)
-	soup = BeautifulSoup(response.text)
-	entities = soup.find_all(tag)
-    
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text)
+    entities = soup.find_all(tag)
+
     if isinstance(attr, dict):
         entities = soup.find(tag, attr)
-	return entities
+    return entities
