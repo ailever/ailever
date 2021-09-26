bab785693329d69fa2d3fe7f2c86dd8c55651ea9
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-09-26 14:39:27 +0900

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name                = 'ailever',
-      version             = '0.3.153',
+      version             = '0.3.154',
       description         = 'Clever Artificial Intelligence',
       author              = 'ailever',
       author_email        = 'ailever.group@gmail.com',
