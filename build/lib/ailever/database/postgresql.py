f20bf3824693b498934cc98eefbfd6405cefbcdc
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-10-03 20:12:54 +0900

    update

--- build/lib/ailever/database/postgresql.py
+++ build/lib/ailever/database/postgresql.py
@@ -3,15 +3,19 @@ from .__db_access_definition import DatabaseAccessObject
 import os
 import pandas as pd
 
+installation = """
+$ sudo apt update
+$ sudo apt install postgresql postgresql-contrib
+$ sudo service --status-all
+$ sudo service postgresql start
+"""
+
 class PostgreSQL(DatabaseAccessObject):
     def __init__(self, verbose=False):
         if verbose:
             self.installation_guide()
 
     def installation_guide(self):
-        manual_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'installation_postgresql')
-        with open(manual_path, 'r') as file:
-            installation = file.read()
         print(installation)
 
     def meta_information(self):
