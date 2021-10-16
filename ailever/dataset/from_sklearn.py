7201ee598801292b2cb2962076adf2b2219460db
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-10-16 21:06:50 +0900

    update

--- ailever/dataset/from_sklearn.py
+++ ailever/dataset/from_sklearn.py
@@ -3,54 +3,54 @@ import pandas as pd
 class Sklearn_API:
     def boston(self, download=False):
         from sklearn.datasets import load_boston
-        X, y = load_boston(return_X_y=True)
-        df = pd.DataFrame(data=X)
-        df['target'] = y
+        loaded_dataset = load_boston(return_X_y=False)
+        df = pd.DataFrame(data=loaded_dataset['data'], columns=loaded_dataset['feature_names'])
+        df['target'] = loaded_dataset['target']
         if download:
             df.to_csv('boston.csv')
         return df
 
     def breast_cancer(self, download=False):
         from sklearn.datasets import load_breast_cancer
-        X, y = load_breast_cancer(return_X_y=True)
-        df = pd.DataFrame(data=X)
-        df['target'] = y
+        loaded_dataset = load_breast_cancer(return_X_y=False)
+        df = pd.DataFrame(data=loaded_dataset['data'], columns=loaded_dataset['feature_names'])
+        df['target'] = loaded_dataset['target']
         if download:
             df.to_csv('breast_cancer.csv')
         return df
 
     def diabetes(self, download=False):
         from sklearn.datasets import load_diabetes
-        X, y = load_diabetes(return_X_y=True)
-        df = pd.DataFrame(data=X)
-        df['target'] = y
+        loaded_dataset = load_diabetes(return_X_y=False)
+        df = pd.DataFrame(data=loaded_dataset['data'], columns=loaded_dataset['feature_names'])
+        df['target'] = loaded_dataset['target']
         if download:
             df.to_csv('diabetes.csv')
         return df
 
     def digits(self, download=False):
         from sklearn.datasets import load_digits
-        X, y = load_digits(return_X_y=True)
-        df = pd.DataFrame(data=X)
-        df['target'] = y
+        loaded_dataset = load_digits(return_X_y=False)
+        df = pd.DataFrame(data=loaded_dataset['data'], columns=loaded_dataset['feature_names'])
+        df['target'] = loaded_dataset['target']
         if download:
             df.to_csv('digits.csv')
         return df
 
     def iris(self, download=False):
         from sklearn.datasets import load_iris
-        X, y = load_iris(return_X_y=True)
-        df = pd.DataFrame(data=X)
-        df['target'] = y
+        loaded_dataset = load_iris(return_X_y=False)
+        df = pd.DataFrame(data=loaded_dataset['data'], columns=loaded_dataset['feature_names'])
+        df['target'] = loaded_dataset['target']
         if download:
             df.to_csv('iris.csv')
         return df
 
     def wine(self, download=False):
         from sklearn.datasets import load_wine
-        X, y = load_wine(return_X_y=True)
-        df = pd.DataFrame(data=X)
-        df['target'] = y
+        loaded_dataset = load_wine(return_X_y=False)
+        df = pd.DataFrame(data=loaded_dataset['data'], columns=loaded_dataset['feature_names'])
+        df['target'] = loaded_dataset['target']
         if download:
             df.to_csv('wine.csv')
         return df
