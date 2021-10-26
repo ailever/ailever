dc7ba547461789038d13d9459addbdb5ef1018d4
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-10-26 20:39:23 +0900

    update

--- build/lib/ailever/dataset/__init__.py
+++ build/lib/ailever/dataset/__init__.py
@@ -1,6 +1,7 @@
 from .DATASETInterfaces import Loader
 from .DATASETInterfaces import SMAPI
 from .DATASETInterfaces import SKAPI
+from .DATASETInterfaces import AILAPI
 from .DATASETInterfaces import UCI
 
 
--- build/lib/ailever/dataset/from_ailever.py
+++ build/lib/ailever/dataset/from_ailever.py
@@ -1,4 +1,17 @@
+import pandas as pd
 
 class Integrated_Loader:
     def __init__(self):
         pass
+
+def Ailever_API(meta_info=True, table:str=None, download=False):
+    if table is None:
+        meta_table = pd.DataFrame(
+                columns=['TABLE', 'DESCRIPTION'], 
+                data=[['district0001', 'korea administrative district'], 
+                    ])
+        return meta_table
+    else:
+        table = pd.read_csv('https://raw.githubusercontent.com/ailever/dataset/main/{table.upper()}.csv', index_col=0)
+        return table
+    
