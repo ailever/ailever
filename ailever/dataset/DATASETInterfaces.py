b06c305fd6357d7ede3e62adab8b0f153a39a4d4
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-10-26 20:46:10 +0900

    update

--- ailever/dataset/DATASETInterfaces.py
+++ ailever/dataset/DATASETInterfaces.py
@@ -4,8 +4,9 @@ from .from_sklearn import Sklearn_API
 from .from_ailever import Ailever_API
 from .uci_machine_learning_repository import UCI_MLR
 
+AILAPI = Ailever_API
+
 Loader = Integrated_Loader()
 SMAPI = Statsmodels_API()
 SKAPI = Sklearn_API()
-AILAPI = Ailever_API()
 UCI = UCI_MLR()
