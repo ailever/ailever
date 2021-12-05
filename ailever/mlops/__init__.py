8d4b1905bed19bfc024b15af273f41aef5a368a6
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-12-05 23:21:29 +0900

    update

--- ailever/mlops/__init__.py
+++ ailever/mlops/__init__.py
@@ -7,3 +7,19 @@ local_environment['model_registry'] = 'model_registry'
 local_environment['source_repository'] = 'model_repository'
 local_environment['metadata_store'] = 'metadata_store'
 __mlops_bs__ = initialization_policy(local_environment)
+
+
+def project(local_environment:dict=None):
+    if local_environment is None:
+        local_environment = dict()
+        local_environment['root'] = '.mlops'
+        local_environment['feature_store'] = 'feature_store'
+        local_environment['model_registry'] = 'model_registry'
+        local_environment['source_repository'] = 'model_repository'
+        local_environment['metadata_store'] = 'metadata_store'
+    else:
+        assert isinstance(local_environment, dict), 'The local_environment must be an instance of dictionary.'
+    return initialization_policy(local_environment)
+
+    
+
