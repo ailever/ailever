65d4e10101575293c42a61d4fc2e34e0f28acfff
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2021-12-21 04:52:18 +0900

    update

--- build/lib/ailever/mlops/mlops.py
+++ build/lib/ailever/mlops/mlops.py
@@ -3,7 +3,9 @@ from importlib import import_module
 import os
 import re
 from datetime import datetime
+from shutil import copyfile
 from copy import deepcopy
+import numpy as np
 import pandas as pd
 import joblib
 
@@ -27,6 +29,14 @@ class Framework(metaclass=ABCMeta):
 
     @abstractmethod
     def upload(self):
+        pass
+ 
+    @abstractmethod
+    def save_dataset(self):
+        pass
+
+    @abstractmethod
+    def save_model(self):
         pass
 
     @abstractmethod
@@ -56,27 +66,29 @@ class FrameworkSklearn(Framework):
         model_class = getattr(import_module(supported_framework+'.'+module_name), model_name)
         return model_class
 
-    def train(self, model, dataset, mlops_path, saving_name):
-        training_info_detail = dict()
-        training_start_time = datetime.today().strftime('%Y%m%d_%H%M%S')
+    def train(self, model, dataset):
         X = dataset.loc[:, dataset.columns != 'target']
         y = dataset.loc[:, 'target'].ravel()
         model.fit(X, y)
-        training_end_time = datetime.today().strftime('%Y%m%d_%H%M%S')
-        
-        saving_name = training_end_time + '-' + f'{saving_name}.joblib'
-        model_registry_path = os.path.join(mlops_path, saving_name)
-        joblib.dump(model, model_registry_path)
-        training_info_detail['training_start_time'] = training_start_time
-        training_info_detail['training_end_time'] = training_end_time
-        training_info_detail['saving_model_name'] = saving_name
-        return model, training_info_detail
+        return model
 
     def predict(self, model, X):
         return model.predict(X)
 
     def upload(self, model_registry_path):
         return joblib.load(model_registry_path)
+
+    def save_dataset(self):
+        pass
+
+    def save_model(self, model, mlops_path, saving_name):
+        saving_name = saving_name + '.joblib'
+        model_registry_saving_path = os.path.join(mlops_path, saving_name)
+        joblib.dump(model, model_registry_saving_path)
+
+        training_info_detail = dict()
+        training_info_detail['saving_model_name'] = saving_name
+        return training_info_detail
 
     def save_outsidemodel(self, model, model_registry_path, outsidelog_path):
         extension = '.joblib'
@@ -226,22 +238,22 @@ class MLTrigger:
         self.catboost = FrameworkCatboost()
         self.supported_frameworks = ['sklearn', 'xgboost', 'lightgbm', 'catboost']
 
-    def preprocessing(self):
-        if not isinstance(self._user_datasets, list):
+    def preprocessing(self, entry_point=None):
+        if not isinstance(self._user_datasets, (list, tuple)):
             self._user_datasets = [self._user_datasets]
 
         self.preprocessing_information = list()
         for idx_dataset, dataset in enumerate(self._user_datasets):
-            dataset_name = f'dataset{idx_dataset}.csv'
             saving_time = datetime.today().strftime('%Y%m%d_%H%M%S')
+            dataset_name = f'dataset{idx_dataset}.csv'
             dataset_saving_name = saving_time + '-' + dataset_name
             saving_path = os.path.join(self.core['FS'].path, dataset_saving_name)
-            
+
             dataset.to_csv(saving_path, index=False)
-            self.preprocessing_information.append((idx_dataset, dataset_saving_name, saving_time))
+            self.preprocessing_information.append((idx_dataset, dataset_saving_name, saving_time, entry_point))
+        return dataset # last dataset
 
-
-    def learning(self):
+    def learning(self, entry_point=None):
         if not isinstance(self._user_models, list):
             self._user_models = [self._user_models]
         
@@ -258,7 +270,20 @@ class MLTrigger:
                             if isinstance(user_model, getattr(self, supported_framework).get_model_class(supported_framework, module_name, model_name)):
                                 framework_name = supported_framework
                                 framework = getattr(self, framework_name)
-                                model, training_info_detail = framework.train(user_model, dataset, mlops_path=self.core['MR'].path, saving_name=model_name)
+                                
+                                # training job
+                                trainingjob_start_time = datetime.today().strftime('%Y%m%d_%H%M%S')
+                                if not entry_point:
+                                    model = framework.train(user_model, dataset)
+                                else:
+                                    model = self.entry_point['train'](user_model, dataset)
+
+                                trainingjob_end_time = datetime.today().strftime('%Y%m%d_%H%M%S')
+                                saving_name = trainingjob_end_time + '-' + f'{model_name}'
+                                training_info_detail = framework.save_model(model, mlops_path=self.core['MR'].path, saving_name=model_name)
+                                training_info_detail['training_start_time'] = trainingjob_start_time
+                                training_info_detail['training_end_time'] = trainingjob_end_time
+
                                 _break_l1 = True
                                 break
                         if _break_l1:
@@ -275,12 +300,18 @@ class MLTrigger:
                         self._training_info_detail = training_info_detail
                         break
                 self.training_information['L1'].append((
-                    self._model_idx, self._dataset_idx, self._model_name, self._framework_name, deepcopy(self._model), self._framework,
-                    self._training_info_detail['training_start_time'], self._training_info_detail['training_end_time'], self._training_info_detail['saving_model_name']))
+                    self._model_idx, self._dataset_idx, self._model_name, self._framework_name, 
+                    deepcopy(self._model), self._framework, 
+                    self._training_info_detail['training_start_time'], 
+                    self._training_info_detail['training_end_time'], 
+                    self._training_info_detail['saving_model_name']))
 
-        info_train = pd.DataFrame(list(map(lambda x: (x[0], x[1], x[2], x[3], x[6], x[7], x[8]), self.training_information['L1'])), 
-                columns=['t_idx', 'd_idx', 'model_name', 'framework_name', 't_start_time', 't_end_time', 't_saving_name'])
-        info_dataset = pd.DataFrame(self.preprocessing_information, columns=['d_idx', 'd_saving_name', 'd_saving_time'])
+        info_train = pd.DataFrame(
+                data=list(map(lambda x: (x[0], x[1], x[2], x[3], x[6], x[7], x[8]), self.training_information['L1'])), 
+                columns=self._insidelog_entities['train'])
+        info_dataset = pd.DataFrame(
+                data=self.preprocessing_information, 
+                columns=self._insidelog_entities['preprocessing'])
 
         self.inside_board = pd.merge(info_train, info_dataset, how='left', on='d_idx')
         self.inside_board['from'] = 'inside'
@@ -295,7 +326,7 @@ class MLTrigger:
         self._model = self.training_information['L1'][0][4]
         return self._model
 
-    def prediction(self, X):
+    def prediction(self, X, code=None):
         return self._framework.predict(self._model, X)
 
     def get_model(self, model_registry_path):
@@ -310,24 +341,36 @@ class MLOps(MLTrigger):
     def __init__(self, mlops_bs):
         super(MLOps, self).__init__()
         self.core = mlops_bs.core
-        self._insidelog_name = 'mlops_insidelog.csv'
-        self._outsidelog_name = 'mlops_outsidelog.csv'
+        self._entry_point = False
         self.__dataset = None
         self.__model = None
-    
+        
+        # usage with the 'inside_board' definition inside the 'def learning' on the class 'MLTrigger'
+        # self.inside_board > self.insidelog
+        self._insidelog_name = 'mlops_insidelog.csv'
+        self._insidelog_entities = dict() # columns of insidelog
+        self._insidelog_entities['preprocessing'] = ['d_idx', 'd_saving_name', 'd_saving_time'] + ['c_entry_point']
+        self._insidelog_entities['train'] = ['t_idx', 'd_idx', 'model_name', 'framework_name', 't_start_time', 't_end_time', 't_saving_name']
+        self._insidelog_entitiew['logtype'] = ['from']
+        self._insidelog_columns = self._insidelog_entities['train'] + self._insidelog_entities['preprocessing'][1:] + self._insidelog_entitiew['logtype']
+        
+        # usage with the 'appending_board' definition inside the 'def storing_model'
+        self._outsidelog_name = 'mlops_outsidelog.csv'
+        self._outsidelog_columns = ['t_idx', 'model_name', 'framework_name', 't_saving_name', 'from', 'comment']
+
         logging_history = list(filter(lambda x: re.search(self._insidelog_name[:-4], x), self.core['MS'].listdir()))
         logging_path = os.path.join(self.core['MS'].path, self._insidelog_name)
         if bool(len(logging_history)):
             self.insidelog = pd.read_csv(logging_path)
         else:
-            self.insidelog = pd.DataFrame(columns=['t_idx', 'd_idx', 'model_name', 'framwork_name', 't_start_time', 't_end_time', 't_saving_name', 'd_saving_name', 'd_saving_time', 'from'])
+            self.insidelog = pd.DataFrame(columns=self._insidelog_columns)
 
         logging_history = list(filter(lambda x: re.search(self._outsidelog_name[:-4], x), self.core['MS'].listdir()))
         logging_path = os.path.join(self.core['MS'].path, self._outsidelog_name)
         if bool(len(logging_history)):
             self.outsidelog = pd.read_csv(logging_path)
         else:
-            self.outsidelog = pd.DataFrame(columns=['t_idx', 'model_name', 'framework_name', 't_saving_name', 'from', 'comment'])
+            self.outsidelog = pd.DataFrame(columns=self._outsidelog_columns)
 
     @property
     def dataset(self):
@@ -396,7 +439,6 @@ class MLOps(MLTrigger):
             pass
         return self
 
-
     def drawup_dataset(self, name):
         return pd.read_csv(os.path.join(self.core['FS'].path, name))
 
@@ -436,17 +478,28 @@ class MLOps(MLTrigger):
         model_registry_path = os.path.join(self.core['MR'].path, saving_time + '-' + model_name)
 
         appending_board = pd.DataFrame(
-                columns=['t_idx',                 'framework_name', 'model_name', 't_saving_name',                'from', 'comment'],
-                data=  [[self.outsidelog.shape[0], self._framework_name, model_name,  saving_time + '-' + model_name, 'outside', comment]])
+                data=  [[self.outsidelog.shape[0], model_name, self._framework_name, saving_time + '-' + model_name, 'outside', comment]])
+                columns=self._outsidelog_columns,
         self.outsidelog = appending_board.append(self.outsidelog, ignore_index=True)
         self.outsidelog.to_csv(os.path.join(self.core['MS'].path, self._outsidelog_name), index=False)
         self.put_model(user_model, model_registry_path)
         self.outsidelog = pd.read_csv(os.path.join(self.core['MS'].path, self._outsidelog_name))
 
     def codecommit(self, entry_point):
-        preprocessing = getattr(import_module(entry_point), 'preprocessing')
-        train = getattr(import_module(entry_point), 'train')
-        predict = getattr(import_module(entry_point), 'predict')
+        mlops_entry_point = datetime.today().strftime('%Y%m%d_%H%M%S') + '-' + entry_point
+        copyfile(entry_point, os.path.join(self.core['SR'].path, mlops_entry_point))
 
+        self.entry_point = dict()
+        entry_name = entry_point[:-3] # *.py
+        self.entry_point['architecture'] = getattr(import_module(entry_name), 'architecture')    # return user_models
+        self.entry_point['preprocessing'] = getattr(import_module(entry_name), 'preprocessing')  # return datasets
+        self.entry_point['train'] = getattr(import_module(entry_name), 'train')                  # return model
+        self.entry_point['predict'] = getattr(import_module(entry_name), 'predict')
+
+        self._user_datasets = self.entry_point['preprocessing']()
+        self.__dataset = self.preprocessing(entry_point=mlops_entry_point)
+        self._user_models = self.entry_point['architecuture']()
+        self.__model = self.learning(entry_point=mlops_entry_point)
+        
     def summary(self):
         return
