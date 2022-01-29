a0364dc4b73b5f75f7e60230ce43d264ade89ff0
Dongmyeong-Lee <dongmyeong.6626070@gmail.com>
2022-01-29 19:32:37 +0900

    update

--- build/lib/ailever/forecast/stockprophets/__init__.py
+++ build/lib/ailever/forecast/stockprophets/__init__.py
@@ -41,29 +41,24 @@ class StockProphet:
 
         return self.evaluation
 
-    def simulate(self, model_name, code, max_lag, sequence_length, trainstartdate, invest_begin):
+    def simulate(self, model_name, code, min_lag, max_lag, sequence_length, trainstartdate, invest_begin):
         if sequence_length is None:
             sequence_length = self.sequence_length
 
         results = list()
-        for lag_shift in range(1, max_lag):
-            self.evaluation = self.MainForecaster.validate(model_name=model_name, trainstartdate=trainstartdate, teststartdate=invest_begin, code=code, lag_shift=lag_shift, sequence_length=sequence_length, comment=None, visual_on=False)
+        accounts = dict()
+        for lag_shift in range(min_lag, max_lag):
+            evaluation = self.MainForecaster.validate(model_name=model_name, trainstartdate=trainstartdate, teststartdate=invest_begin, code=code, lag_shift=lag_shift, sequence_length=sequence_length, comment=None, visual_on=False)
 
-            self.dataset = self.MainForecaster.dataset.copy()
-            self.price = self.MainForecaster.price.copy()
-            self.X = self.MainForecaster.X.copy()
-            self.y = self.MainForecaster.y.copy()
-            self.model = self.MainForecaster.model
+            dataset = self.MainForecaster.dataset.copy()
+            price = self.MainForecaster.price.copy()
+            X = self.MainForecaster.X.copy()
+            y = self.MainForecaster.y.copy()
+            model = self.MainForecaster.model
 
-            if code is not None:
-                self.code = code
-            if lag_shift is not None:
-                self.lag_shift = lag_shift
-            if sequence_length is not None:
-                self.sequence_length = sequence_length
-
-            account = pd.DataFrame(data=np.c_[self.price.loc[invest_begin:].values.squeeze(), self.model.predict(self.X.loc[invest_begin:]).squeeze()], index=self.X.loc[invest_begin:].index.copy(), columns=['Price', 'Decision'])
+            account = pd.DataFrame(data=np.c_[price.loc[invest_begin:].values.squeeze(), model.predict(X.loc[invest_begin:]).squeeze()], index=X.loc[invest_begin:].index.copy(), columns=['Price', 'Decision'])
             account['LagShift'] = lag_shift
+            account['SequenceLength'] = sequence_length
             account = account.assign(Buy=lambda x: - x.Price * x.Decision)
             account = account.assign(Sell=lambda x: x.Price * (x.Decision*(-1)+1))
             account['Cash'] = account.assign(Cash=lambda x: x.Buy + x.Sell).Cash.cumsum() - account.Sell.astype(bool).sum()*account.Price[0]
@@ -72,9 +67,11 @@ class StockProphet:
             invest = - account['Cash'][0]
             margin = account.Buy.astype(bool).sum()*account.Price[-1] - account.Sell.astype(bool).sum()*account.Price[0] + account.Sell.sum() + account.Buy.sum()
             profit = margin / invest
-            invest_end = self.X.index[-1].strftime('%Y-%m-%d')
+            invest_end = X.index[-1].strftime('%Y-%m-%d')
             results.append([code, invest_begin, invest_end, lag_shift, margin, invest, profit])
+            accounts[lag_shift] = account
         report = pd.DataFrame(data=results, columns=['Code', 'Start', 'End', 'LagShift', 'Margin', 'Invest', 'Profit'])
+        self.accounts = accounts
         self.account = account
         self.report = report
         return report
